0cfbeb3b13b1982df160d95dc82d6f0e34bbf533
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-21 15:26:43 +0900

    init

---
+++ backend/src/test/java/com/reviewping/coflo/CofloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewping.coflo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
