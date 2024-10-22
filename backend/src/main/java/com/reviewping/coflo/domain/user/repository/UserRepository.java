5d84cfb993816e09fd8ca2017fcbdde5a2be3f64
유승현 <fview@naver.com>
2024-10-22 14:45:17 +0900

    refactor: 불필요한 코드 삭제

--- backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.reviewping.coflo.domain.user.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reviewping.coflo.domain.user.entity.User;
