66f612dac9d66d6b68cae832beb551e15114ae52
손지민 <jimmi219@naver.com>
2024-10-22 10:17:30 +0900

    feat: 기본 예외 처리 추가

---
+++ backend/src/main/java/com/reviewping/coflo/global/error/ErrorCode.java
@@ -0,0 +1,33 @@
+package com.reviewping.coflo.global.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+	/**
+	 * USER(A000)
+	 */
+	USER_NOT_EXIST(HttpStatus.BAD_REQUEST, "A001", "존재하지 않는 유저입니다"),
+
+	/**
+	 * JWT Token(H000)
+	 */
+	TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "H001", "유효하지 않은 토큰입니다."),
+	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "H002", "만료된 토큰입니다."),
+	TOKEN_UNSUPPORTED(HttpStatus.UNAUTHORIZED, "H003", "지원되지 않는 토큰입니다."),
+	TOKEN_WRONG(HttpStatus.UNAUTHORIZED, "H004", "잘못된 토큰 서명입니다."),
+
+	/**
+	 * Etc(Z000)
+	 */
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Z009", "잘못된 파라미터가 포함되었습니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+}
