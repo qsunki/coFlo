9b3c0927e760b7661c975fcb29111b161584d6f5
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-22 15:06:24 +0900

    feat: RedisUtil 생성

---
+++ backend/src/main/java/com/reviewping/coflo/global/util/RedisUtil.java
@@ -0,0 +1,30 @@
+package com.reviewping.coflo.global.util;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	public void set(String key, Object o, int minutes) {
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(o.getClass()));
+		redisTemplate.opsForValue().set(key, o, minutes, TimeUnit.MINUTES);
+	}
+
+	public Object get(String key) {
+		return redisTemplate.opsForValue().get(key);
+	}
+
+	public boolean delete(String key) {
+		return Boolean.TRUE.equals(redisTemplate.delete(key));
+	}
+
+}
