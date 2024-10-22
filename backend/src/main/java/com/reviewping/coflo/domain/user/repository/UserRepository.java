3b86982056ca541586381e8e52d470d7cd3db7f5
유승현 <fview@naver.com>
2024-10-22 14:36:45 +0900

    feat: user repository exists by oauth2 id 추가

--- backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.reviewping.coflo.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.reviewping.coflo.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Boolean existsByOauth2Id(String oauth2Id);
 }
