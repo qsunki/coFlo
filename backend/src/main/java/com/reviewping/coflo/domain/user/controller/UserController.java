892a733d791135b335aabc3e6c9ef5f4b88008a3
유승현 <fview@naver.com>
2024-10-23 08:57:25 +0900

    fix: requestbody 어노테이션 누락 수정
    Jira: S11P31A210-237

--- backend/src/main/java/com/reviewping/coflo/domain/user/controller/UserController.java
+++ backend/src/main/java/com/reviewping/coflo/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.reviewping.coflo.domain.user.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class UserController {
 	@PostMapping("/me")
 	public ApiResponse<Void> addGitlabAccount(
 		@AuthenticationPrincipal User user,
-		GitlabAccountRequest gitlabAccountRequest) {
+		@RequestBody GitlabAccountRequest gitlabAccountRequest) {
 		userService.addGitlabAccount(gitlabAccountRequest.domain(),
 			gitlabAccountRequest.userToken(), user.getOauth2Id());
 		return ApiSuccessResponse.success();
