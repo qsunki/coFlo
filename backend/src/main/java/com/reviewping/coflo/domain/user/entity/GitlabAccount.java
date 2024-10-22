b982023a2650232c02650aa46ddbaea9dd16c038
유승현 <fview@naver.com>
2024-10-22 15:08:18 +0900

    feat: gitlab account entity 생성

---
+++ backend/src/main/java/com/reviewping/coflo/domain/user/entity/GitlabAccount.java
@@ -0,0 +1,39 @@
+package com.reviewping.coflo.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GitlabAccount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column(nullable = false)
+	private String domain;
+
+	@Column(nullable = false, unique = true)
+	private String userToken;
+
+	@Builder
+	public GitlabAccount(User user, String domain, String userToken) {
+		this.user = user;
+		this.domain = domain;
+		this.userToken = userToken;
+	}
+}
