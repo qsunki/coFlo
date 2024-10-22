3bdb71bc76a822d9114ce13413f99f52e7f0e7e7
유승현 <fview@naver.com>
2024-10-23 08:56:52 +0900

    feat: gitlab user info response dto 추가
    Jira: S11P31A210-237

---
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/dto/response/GitlabUserInfoResponse.java
@@ -0,0 +1,11 @@
+package com.reviewping.coflo.domain.gitlab.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record GitlabUserInfoResponse(
+	String username,
+	String avatarUrl
+) {
+}
