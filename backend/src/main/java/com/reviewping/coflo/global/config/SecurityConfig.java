3de61e07a1e2e2e2c41ada465be6137d7fabf99e
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-22 17:15:05 +0900

    feat: security 설정 정보

---
+++ backend/src/main/java/com/reviewping/coflo/global/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.reviewping.coflo.global.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.reviewping.coflo.global.jwt.filter.JwtVerifyFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public JwtVerifyFilter jwtVerifyFilter() {
+		return new JwtVerifyFilter();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http.csrf(AbstractHttpConfigurer::disable);
+
+		http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+			httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		});
+
+		// CORS 설정 추가
+		http.cors(cors -> {
+			cors.configurationSource(corsConfigurationSource());
+		});
+
+		http.addFilterBefore(jwtVerifyFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		http.formLogin(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList(
+			"http://localhost:5173",
+			"https://localhost:5173")); // 허용할 도메인 설정
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메소드
+		configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+		configuration.setAllowCredentials(true); // 자격 증명 허용
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
