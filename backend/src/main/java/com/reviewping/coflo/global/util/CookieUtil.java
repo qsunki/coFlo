38c61cd8f97cf9a09b11f6fdf6d4451ece8ede05
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-23 21:17:15 +0900

    feat: CookieUtil 추가
    Jira: S11P31A210-179

---
+++ backend/src/main/java/com/reviewping/coflo/global/util/CookieUtil.java
@@ -0,0 +1,31 @@
+package com.reviewping.coflo.global.util;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public Cookie createCookie(
+            String name, String value, int maxAge, boolean httpOnly, boolean secure, String path) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setHttpOnly(httpOnly);
+        cookie.setSecure(secure);
+        cookie.setPath(path);
+        cookie.setMaxAge(maxAge);
+        return cookie;
+    }
+
+    public String getCookieValue(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(cookieName)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
