7007547635fa170f3fb46b126dc9b4f95c012d3c
손지민 <jimmi219@naver.com>
2024-10-23 09:12:45 +0900

    feat: gitlab api endpoint search로 수정
    Jira: S11P31A210-235

--- backend/src/main/java/com/reviewping/coflo/domain/gitlab/service/GitLabApiService.java
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/service/GitLabApiService.java
@@ -1,47 +1,55 @@
 package com.reviewping.coflo.domain.gitlab.service;
 
-import com.reviewping.coflo.domain.gitlab.dto.response.GitlabProjectResponse;
-import com.reviewping.coflo.domain.gitlab.dto.response.GitlabUserInfoResponse;
-import com.reviewping.coflo.global.util.RestTemplateUtils;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.reviewping.coflo.domain.gitlab.dto.response.GitlabProjectResponse;
+import com.reviewping.coflo.domain.gitlab.dto.response.GitlabUserInfoResponse;
+import com.reviewping.coflo.global.util.RestTemplateUtils;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GitLabApiService {
 
-    @Value("${gitlab.api.project-list}")
-    private String projectListEndpoint;
+	@Value("${gitlab.api.project-search}")
+	private String projectListEndpoint;
 
-    @Value("${gitlab.api.user-info}")
-    private String userInfoEndpoint;
+	@Value("${gitlab.api.user-info}")
+	private String userInfoEndpoint;
 
-    public List<GitlabProjectResponse> getProjects(String gitlabUrl, String token) {
-        HttpHeaders headers = RestTemplateUtils.createHeaders("application/json", token);
-        String url = gitlabUrl + projectListEndpoint;
+	private static final String URL_PROTOCOL_HTTPS = "https://";
+	private static final String MIME_TYPE_JSON = "application/json";
 
-        ResponseEntity<List<GitlabProjectResponse>> response =
-                RestTemplateUtils.sendGetRequest(
-                        url,
-                        headers,
-                        new ParameterizedTypeReference<List<GitlabProjectResponse>>() {});
-        return response.getBody();
-    }
+	public List<GitlabProjectResponse> searchGitlabProjects(String gitlabUrl, String token, String searchQuery) {
+		HttpHeaders headers = RestTemplateUtils.createHeaders(MIME_TYPE_JSON, token);
+		String url = URL_PROTOCOL_HTTPS + gitlabUrl + projectListEndpoint + searchQuery;
 
-    public GitlabUserInfoResponse getUserInfo(String gitlabUrl, String token) {
-        HttpHeaders headers = RestTemplateUtils.createHeaders("application/json", token);
-        String url = "https://" + gitlabUrl + userInfoEndpoint;
+		ResponseEntity<List<GitlabProjectResponse>> response =
+			RestTemplateUtils.sendGetRequest(
+				url,
+				headers,
+				new ParameterizedTypeReference<List<GitlabProjectResponse>>() {
+				});
+		return response.getBody();
+	}
 
-        ResponseEntity<GitlabUserInfoResponse> response =
-                RestTemplateUtils.sendGetRequest(
-                        url, headers, new ParameterizedTypeReference<GitlabUserInfoResponse>() {});
-        return response.getBody();
-    }
+	public GitlabUserInfoResponse getUserInfo(String gitlabUrl, String token) {
+		HttpHeaders headers = RestTemplateUtils.createHeaders(MIME_TYPE_JSON, token);
+		String url = URL_PROTOCOL_HTTPS + gitlabUrl + userInfoEndpoint;
+
+		ResponseEntity<GitlabUserInfoResponse> response =
+			RestTemplateUtils.sendGetRequest(
+				url, headers, new ParameterizedTypeReference<GitlabUserInfoResponse>() {
+				});
+		return response.getBody();
+	}
 }
