3c648e832e1b41f00017e22146bcee5753aae708
손지민 <jimmi219@naver.com>
2024-10-23 15:20:54 +0900

    feat: SoftwareQualityRepository 추가
    Jira: S11P31A210-223

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/SoftwareQualityRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/SoftwareQualityRepository.java
@@ -1,9 +1,9 @@
 package com.reviewping.coflo.domain.userproject.repository;
 
-import com.reviewping.coflo.domain.userproject.entity.UserProject;
+import com.reviewping.coflo.domain.userproject.entity.SoftwareQuality;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserProjectRepository extends JpaRepository<UserProject, Long> {
+public interface SoftwareQualityRepository extends JpaRepository<SoftwareQuality, Long> {
 }
