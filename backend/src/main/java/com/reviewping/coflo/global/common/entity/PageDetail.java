50c1812304e4b5938503b41138d55cc2c5c79c6f
손지민 <jimmi219@naver.com>
2024-10-23 22:14:17 +0900

    feat: PageDetail 추가
    Jira: S11P31A210-197

--- backend/src/main/java/com/reviewping/coflo/global/common/entity/PageDetail.java
+++ backend/src/main/java/com/reviewping/coflo/global/common/entity/PageDetail.java
@@ -1,11 +1,6 @@
 package com.reviewping.coflo.global.common.entity;
 
-public record PageDetail(
-        long totalElements,
-        int totalPages,
-        boolean isLast,
-        int currPage
-) {
+public record PageDetail(long totalElements, int totalPages, boolean isLast, int currPage) {
     public static PageDetail of(long totalElements, int totalPages, boolean isLast, int currPage) {
         return new PageDetail(totalElements, totalPages, isLast, currPage);
     }
