7075beaf12d0527bae4297c40528b8bee5d42813
손지민 <jimmi219@naver.com>
2024-10-23 15:29:39 +0900

    feat: UserBadge 엔티티 추가
    Jira: S11P31A210-223

---
+++ backend/src/main/java/com/reviewping/coflo/domain/user/entity/UserBadge.java
@@ -0,0 +1,35 @@
+package com.reviewping.coflo.domain.user.entity;
+
+import com.reviewping.coflo.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserBadge extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_id")
+    private Badge badge;
+
+    private boolean isSelected;
+
+    @Builder
+    public UserBadge(User user, Badge badge) {
+        this.user = user;
+        this.badge = badge;
+        user.getUserBadges().add(this);
+    }
+
+}
