f07dab97d1e221eebe3696c0c0e6d11ec1d2eddd
손지민 <jimmi219@naver.com>
2024-10-23 17:25:47 +0900

    feat: 깃랩 레포 조회 페이징 추가
    Jira: S11P31A210-197

--- backend/src/main/java/com/reviewping/coflo/domain/link/service/LinkService.java
+++ backend/src/main/java/com/reviewping/coflo/domain/link/service/LinkService.java
@@ -1,21 +1,21 @@
 package com.reviewping.coflo.domain.link.service;
 
+import static com.reviewping.coflo.global.error.ErrorCode.USER_GITLAB_ACCOUNT_NOT_EXIST;
+import static com.reviewping.coflo.global.error.ErrorCode.USER_NOT_EXIST;
+
 import com.reviewping.coflo.domain.gitlab.dto.response.GitlabProjectContent;
 import com.reviewping.coflo.domain.gitlab.service.GitLabApiService;
+import com.reviewping.coflo.domain.link.controller.dto.request.GitlabSearchRequest;
 import com.reviewping.coflo.domain.link.controller.dto.response.GitlabProjectResponse;
 import com.reviewping.coflo.domain.project.repository.ProjectRepository;
 import com.reviewping.coflo.domain.user.entity.GitlabAccount;
 import com.reviewping.coflo.domain.user.repository.UserRepository;
 import com.reviewping.coflo.domain.userproject.repository.UserProjectRepository;
 import com.reviewping.coflo.global.error.exception.BusinessException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.reviewping.coflo.global.error.ErrorCode.USER_GITLAB_ACCOUNT_NOT_EXIST;
-import static com.reviewping.coflo.global.error.ErrorCode.USER_NOT_EXIST;
 
 @Service
 @RequiredArgsConstructor
@@ -27,30 +27,40 @@ public class LinkService {
     private final ProjectRepository projectRepository;
     private final UserProjectRepository userProjectRepository;
 
-    public List<GitlabProjectResponse> getGitlabProjects(Long userId, String keyword) {
+    public List<GitlabProjectResponse> getGitlabProjects(
+            Long userId, GitlabSearchRequest gitlabSearchRequest) {
         GitlabAccount gitlabAccount = findGitlabAccount(userId);
         List<GitlabProjectContent> searchGitlabProjects =
                 gitLabApiService.searchGitlabProjects(
-                        gitlabAccount.getDomain(), gitlabAccount.getUserToken(), keyword);
+                        gitlabAccount.getDomain(),
+                        gitlabAccount.getUserToken(),
+                        gitlabSearchRequest);
         return searchGitlabProjects.stream()
                 .map(project -> createGitlabProjectResponse(project, gitlabAccount.getId()))
                 .toList();
     }
 
     private GitlabAccount findGitlabAccount(Long userId) {
-        return userRepository.findById(userId)
+        return userRepository
+                .findById(userId)
                 .orElseThrow(() -> new BusinessException(USER_NOT_EXIST))
-                .getGitlabAccounts().stream().findFirst()
+                .getGitlabAccounts()
+                .stream()
+                .findFirst()
                 .orElseThrow(() -> new BusinessException(USER_GITLAB_ACCOUNT_NOT_EXIST));
     }
 
-    private GitlabProjectResponse createGitlabProjectResponse(GitlabProjectContent content, Long gitlabAccountId) {
-        return projectRepository.findByGitlabProjectId(content.id())
-                .map(project -> {
-                    boolean isLinked = userProjectRepository.existsByGitlabAccountIdAndProjectId(gitlabAccountId, project.getId());
-                    return GitlabProjectResponse.of(content, true, isLinked);
-                })
+    private GitlabProjectResponse createGitlabProjectResponse(
+            GitlabProjectContent content, Long gitlabAccountId) {
+        return projectRepository
+                .findByGitlabProjectId(content.id())
+                .map(
+                        project -> {
+                            boolean isLinked =
+                                    userProjectRepository.existsByGitlabAccountIdAndProjectId(
+                                            gitlabAccountId, project.getId());
+                            return GitlabProjectResponse.of(content, true, isLinked);
+                        })
                 .orElseGet(() -> GitlabProjectResponse.of(content, false, false));
     }
-
 }
--- backend/src/main/java/com/reviewping/coflo/domain/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/project/repository/ProjectRepository.java
@@ -1,9 +1,8 @@
 package com.reviewping.coflo.domain.project.repository;
 
 import com.reviewping.coflo.domain.project.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findByGitlabProjectId(Long gitlabProjectId);
