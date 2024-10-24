daac824dc4d0069f938ac4ca706a84b8776838c2
이보연 <btothey99@ewhain.net>
2024-10-24 15:30:39 +0900

    feat: 로그인 컴포넌트 구현

---
+++ frontend/src/pages/Login/LoginButton.tsx
@@ -0,0 +1,28 @@
+interface LoginButtonProps {
+  imageName: string;
+  buttonText: string;
+  bgColor: string;
+  onClick: () => void; // 로그인 요청을 처리할 함수
+}
+
+export default function LoginButton({
+  imageName,
+  buttonText,
+  bgColor,
+  onClick,
+}: LoginButtonProps) {
+  return (
+    <button
+      type="button"
+      className={`w-full py-3 flex items-center justify-center relative font-bold rounded-3xl transition-colors h-14 ${bgColor} shadow-md`}
+      onClick={onClick}
+    >
+      <img
+        src={`/images/logo/${imageName}`}
+        alt={`${buttonText} 로고`}
+        className="absolute left-8 w-4 h-4"
+      />
+      <p className="ml-6">{buttonText}</p>
+    </button>
+  );
+}
