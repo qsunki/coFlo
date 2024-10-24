99f2d33f920843126e72709007f891ecbeaecd63
손지민 <jimmi219@naver.com>
2024-10-24 11:24:04 +0900

    test: 깃랩 조회 테스트(작성 중)
    Jira: S11P31A210-197

---
+++ backend/src/test/java/com/reviewping/coflo/domain/link/service/LinkServiceTest.java
@@ -0,0 +1,51 @@
+package com.reviewping.coflo.domain.link.service;
+
+import com.reviewping.coflo.IntegrationTestSupport;
+import com.reviewping.coflo.domain.gitlab.service.GitLabApiService;
+import com.reviewping.coflo.domain.link.controller.dto.request.GitlabSearchRequest;
+import com.reviewping.coflo.domain.project.repository.ProjectRepository;
+import com.reviewping.coflo.domain.user.repository.UserRepository;
+import com.reviewping.coflo.domain.userproject.repository.UserProjectRepository;
+import com.reviewping.coflo.global.error.exception.BusinessException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.reviewping.coflo.global.error.ErrorCode.USER_NOT_EXIST;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class LinkServiceTest extends IntegrationTestSupport {
+
+    @Autowired
+    private LinkService linkService;
+    @Autowired
+    private GitLabApiService gitLabApiService;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    private UserProjectRepository userProjectRepository;
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAllInBatch();
+        projectRepository.deleteAllInBatch();
+        userProjectRepository.deleteAllInBatch();
+    }
+
+    @Test
+    @DisplayName("조회하려는 유저가 존재하지 않으면 예외가 발생한다.")
+    public void test() throws Exception {
+        //given
+        Long userId = 1L;
+        GitlabSearchRequest gitlabSearchRequest = GitlabSearchRequest.of("", 1, 10);
+
+        //when
+        //then
+        assertThatThrownBy(() -> linkService.getGitlabProjects(userId, gitlabSearchRequest))
+                .isInstanceOf(BusinessException.class)
+                .hasMessage(USER_NOT_EXIST.getMessage());
+    }
+}
