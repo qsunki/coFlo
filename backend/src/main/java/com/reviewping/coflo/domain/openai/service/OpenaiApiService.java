d2d5ba0a87d2120e0c1cf9b4f60d0b678b0633a7
qsunki <ajsthfldu@outlook.com>
2024-10-24 17:43:00 +0900

    fix: openai api 호출시 빠진 헤더 추가
    Jira: S11P31A210-162

--- backend/src/main/java/com/reviewping/coflo/domain/openai/service/OpenaiApiService.java
+++ backend/src/main/java/com/reviewping/coflo/domain/openai/service/OpenaiApiService.java
@@ -1,11 +1,12 @@
 package com.reviewping.coflo.domain.openai.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reviewping.coflo.domain.openai.dto.response.ChatCompletionContent;
 import com.reviewping.coflo.domain.openai.dto.response.ChatCompletionRequest;
 import com.reviewping.coflo.domain.openai.dto.response.ChatMessage;
 import com.reviewping.coflo.global.util.RestTemplateUtils;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -15,16 +16,29 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class OpenaiApiService {
 
     private static final String CHAT_URL = "https://api.openai.com/v1/chat/completions";
+    private final ObjectMapper objectMapper;
+    private final String openaiApiKey;
+    private final String openaiOrganization;
+    private final String openaiProject;
 
-    @Value("${openai.api.key}")
-    private String openaiApiKey;
+    public OpenaiApiService(
+            ObjectMapper objectMapper,
+            @Value("${openai.api.key}") String openaiApiKey,
+            @Value("${openai.api.organization}") String openaiOrganization,
+            @Value("${openai.api.project}") String openaiProject) {
+        this.objectMapper = objectMapper;
+        this.openaiApiKey = openaiApiKey;
+        this.openaiOrganization = openaiOrganization;
+        this.openaiProject = openaiProject;
+    }
 
     public ChatCompletionContent chat(String prompt) {
         HttpHeaders headers = RestTemplateUtils.createHeaders("application/json", openaiApiKey);
+        headers.set("OpenAI-Organization", openaiOrganization);
+        headers.set("OpenAI-Project", openaiProject);
 
         ChatCompletionRequest body =
                 new ChatCompletionRequest("gpt-4o", List.of(new ChatMessage("user", prompt, null)));
