0fcfbfc9b30d933e8b355075c7c5bdeda3e672e9
이보연 <btothey99@ewhain.net>
2024-10-25 17:28:49 +0900

    style: 회원가입 레이아웃 수정

--- frontend/src/pages/Signup/SignupPage.tsx
+++ frontend/src/pages/Signup/SignupPage.tsx
@@ -2,9 +2,11 @@ import SignupContent from '@components/Signup/SignupContent.tsx';
 
 const SignupPage = () => {
   return (
-    <>
-      <SignupContent />
-    </>
+    <div className="w-full h-screen flex justify-start items-center">
+      <div className="w-full max-w-md p-5">
+        <SignupContent />
+      </div>
+    </div>
   );
 };
 
