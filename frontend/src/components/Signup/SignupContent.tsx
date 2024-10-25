646bd662a5abb323477d08f88a07da46ac60e0ea
이보연 <btothey99@ewhain.net>
2024-10-25 12:47:57 +0900

    chore: 회원가입 배경 이미지 설정

---
+++ frontend/src/components/Signup/SignupContent.tsx
@@ -0,0 +1,13 @@
+const SignupContent = () => {
+  return (
+    <>
+      <img
+        src="/images/signup_bg_image.webp"
+        alt="sign up"
+        className="w-screen h-screen"
+      />
+    </>
+  );
+};
+
+export default SignupContent;
