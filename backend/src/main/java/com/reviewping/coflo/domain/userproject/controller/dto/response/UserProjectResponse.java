a1890f1f7018b80b4f831390b4c1f3671075c163
손지민 <jimmi219@naver.com>
2024-10-29 08:59:08 +0900

    refactor: response 응답 변수명 수정
    Jira: S11P31A210-313

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/controller/dto/response/UserProjectResponse.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/controller/dto/response/UserProjectResponse.java
@@ -2,7 +2,7 @@ package com.reviewping.coflo.domain.userproject.controller.dto.response;
 
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
 
-public record UserProjectResponse(Long id, String name) {
+public record UserProjectResponse(Long projectId, String name) {
 
     public static UserProjectResponse of(UserProject userProject) {
         return new UserProjectResponse(
