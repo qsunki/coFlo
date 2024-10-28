613dac611541566636451d784e5bd9048608f9ef
손지민 <jimmi219@naver.com>
2024-10-28 10:23:55 +0900

    feat: 사용자의 연동된 프로젝트가 존재하는지 여부 확인 기능 구현
    Jira: S11P31A210-310

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
@@ -1,9 +1,12 @@
 package com.reviewping.coflo.domain.userproject.repository;
 
+import com.reviewping.coflo.domain.user.entity.User;
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProjectRepository
         extends JpaRepository<UserProject, Long>, UserProjectRepositoryCustom {
     Boolean existsByGitlabAccountIdAndProjectId(Long gitlabAccountId, Long projectId);
+
+    Boolean existsByUser(User user);
 }
