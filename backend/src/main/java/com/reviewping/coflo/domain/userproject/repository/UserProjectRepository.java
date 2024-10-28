6b057d7d842ccb817a957e8ddc5bbeb334ab667e
손지민 <jimmi219@naver.com>
2024-10-28 12:21:39 +0900

    refactor: default 메서드를 활용한 findById 예외 처리
    Jira: S11P31A210-310

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
@@ -1,11 +1,10 @@
 package com.reviewping.coflo.domain.userproject.repository;
 
-import com.reviewping.coflo.domain.user.entity.User;
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProjectRepository extends JpaRepository<UserProject, Long> {
     Boolean existsByGitlabAccountIdAndProjectId(Long gitlabAccountId, Long projectId);
 
-    Boolean existsByGitlabAccountUser(User user);
+    Boolean existsByGitlabAccountUserId(Long userId);
 }
