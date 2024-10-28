514dbf2cc875653bed257801b6736321e2166b25
손지민 <jimmi219@naver.com>
2024-10-28 18:49:16 +0900

    feat: MrInfo 수정 날짜 기준으로 사용자 프로젝트 조회 및 정렬
    Jira: S11P31A210-313

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
@@ -3,7 +3,8 @@ package com.reviewping.coflo.domain.userproject.repository;
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserProjectRepository extends JpaRepository<UserProject, Long> {
+public interface UserProjectRepository
+        extends JpaRepository<UserProject, Long>, UserProjectRepositoryCustom {
     Boolean existsByGitlabAccountIdAndProjectId(Long gitlabAccountId, Long projectId);
 
     Boolean existsByGitlabAccountUserId(Long userId);
