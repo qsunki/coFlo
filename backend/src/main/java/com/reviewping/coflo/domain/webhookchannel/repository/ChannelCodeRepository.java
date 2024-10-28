752d041a62b2860f1553b85aaba324026cfd4158
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-28 10:08:21 +0900

    feat: 웹훅 채널 코드 로직
    Jira: S11P31A210-181

---
+++ backend/src/main/java/com/reviewping/coflo/domain/webhookchannel/repository/ChannelCodeRepository.java
@@ -0,0 +1,6 @@
+package com.reviewping.coflo.domain.webhookchannel.repository;
+
+import com.reviewping.coflo.domain.webhookchannel.entity.ChannelCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelCodeRepository extends JpaRepository<ChannelCode, Long> {}
