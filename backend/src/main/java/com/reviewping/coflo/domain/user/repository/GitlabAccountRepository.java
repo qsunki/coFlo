428be3e0121b006468f2fe5a3cc206700532d48d
유승현 <fview@naver.com>
2024-10-28 10:13:39 +0900

    feat: gitlabmrdetailcontent dto is ai review created 추가
    Jira: S11P31A210-348

--- backend/src/main/java/com/reviewping/coflo/domain/user/repository/GitlabAccountRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/user/repository/GitlabAccountRepository.java
@@ -4,9 +4,17 @@ import com.reviewping.coflo.domain.user.entity.GitlabAccount;
 import com.reviewping.coflo.domain.user.entity.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GitlabAccountRepository extends JpaRepository<GitlabAccount, Long> {
-    Boolean existsByUserId(Long userId);
 
     Optional<GitlabAccount> findFirstByUserOrderByIdAsc(User user);
+
+    @Query(
+            "SELECT ga.id, ga.domain, ga.userToken FROM GitlabAccount ga "
+                    + "JOIN ga.userProjects up "
+                    + "WHERE ga.user.id = :userId AND up.project.id = :projectId")
+    Optional<GitlabAccount> findGitlabAccountByUserIdAndProjectId(
+            @Param("userId") Long userId, @Param("projectId") Long projectId);
 }
