47eae19f8defb85ae045afa7aef1096f732a28db
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-28 15:22:43 +0900

    feat: 엔드포인트 설정
    Jira: S11P31A210-181

--- backend/src/main/java/com/reviewping/coflo/domain/webhookchannel/controller/WebhookChannelController.java
+++ backend/src/main/java/com/reviewping/coflo/domain/webhookchannel/controller/WebhookChannelController.java
@@ -7,9 +7,11 @@ import com.reviewping.coflo.global.common.response.impl.ApiSuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/channels")
 @RequiredArgsConstructor
 public class WebhookChannelController {
 
