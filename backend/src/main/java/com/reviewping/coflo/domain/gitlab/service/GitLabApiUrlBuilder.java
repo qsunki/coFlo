92a7b8e8e320460c62d6ce7e9c36c8b1b5dc8a64
손지민 <jimmi219@naver.com>
2024-10-28 10:04:31 +0900

    refactor: GitlabAPI 호출 서비스와 Url Builder 분리
    Jira: S11P31A210-326

---
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/service/GitLabApiUrlBuilder.java
@@ -0,0 +1,74 @@
+package com.reviewping.coflo.domain.gitlab.service;
+
+import com.reviewping.coflo.domain.link.controller.dto.request.GitlabSearchRequest;
+
+public class GitLabApiUrlBuilder {
+
+    private static final String URL_PROTOCOL_HTTPS = "https://";
+
+    private static final String USERINFO_ENDPOINT = "/api/v4/user";
+    private static final String SINGLE_PROJECT_ENDPOINT = "/api/v4/projects/";
+    private static final String PROJECT_SEARCH_ENDPOINT =
+            "/api/v4/projects?order_by=updated_at&sort=desc";
+
+    public static String createSearchGitlabProjectUrl(
+            String gitlabUrl, GitlabSearchRequest gitlabSearchRequest) {
+        return URL_PROTOCOL_HTTPS
+                + gitlabUrl
+                + PROJECT_SEARCH_ENDPOINT
+                + "&search="
+                + gitlabSearchRequest.keyword()
+                + "&page="
+                + gitlabSearchRequest.page()
+                + "&per_page="
+                + gitlabSearchRequest.size();
+    }
+
+    public static String createMRDiffsUrl(String gitlabUrl, Long gitlabProjectId, Long iid) {
+        return URL_PROTOCOL_HTTPS
+                + gitlabUrl
+                + SINGLE_PROJECT_ENDPOINT
+                + gitlabProjectId
+                + "/merge_requests/"
+                + iid
+                + "/diffs";
+    }
+
+    public static String createNoteToMRUrl(String gitlabUrl, Long gitlabProjectId, Long iid) {
+        return URL_PROTOCOL_HTTPS
+                + gitlabUrl
+                + SINGLE_PROJECT_ENDPOINT
+                + gitlabProjectId
+                + "/merge_requests/"
+                + iid
+                + "/notes";
+    }
+
+    public static String createSearchMergeRequestUrl(
+            String gitlabUrl,
+            Long gitlabProjectId,
+            String mergeRequestState,
+            GitlabSearchRequest gitlabSearchRequest) {
+        return URL_PROTOCOL_HTTPS
+                + gitlabUrl
+                + SINGLE_PROJECT_ENDPOINT
+                + gitlabProjectId
+                + "/merge_requests"
+                + "?state="
+                + mergeRequestState
+                + "&search="
+                + gitlabSearchRequest.keyword()
+                + "&page="
+                + gitlabSearchRequest.page()
+                + "&per_page="
+                + gitlabSearchRequest.size();
+    }
+
+    public static String createSingleProjectUrl(String gitlabUrl, Long gitlabProjectId) {
+        return URL_PROTOCOL_HTTPS + gitlabUrl + SINGLE_PROJECT_ENDPOINT + gitlabProjectId;
+    }
+
+    public static String createUserInfoUrl(String gitlabUrl) {
+        return URL_PROTOCOL_HTTPS + gitlabUrl + USERINFO_ENDPOINT;
+    }
+}
