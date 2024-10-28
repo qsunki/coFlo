f732d290f4d13735391ca864a70e0ac63dc7e973
손지민 <jimmi219@naver.com>
2024-10-28 12:21:39 +0900

    refactor: default 메서드를 활용한 findById 예외 처리
    Jira: S11P31A210-310

--- backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/user/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.reviewping.coflo.domain.user.repository;
 
+import static com.reviewping.coflo.global.error.ErrorCode.USER_NOT_EXIST;
+
 import com.reviewping.coflo.domain.user.entity.User;
+import com.reviewping.coflo.global.error.exception.BusinessException;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByOauth2Id(String oauth2Id);
-
     Optional<User> findByOauth2Id(String oauth2Id);
 
     default User getById(Long userId) {
