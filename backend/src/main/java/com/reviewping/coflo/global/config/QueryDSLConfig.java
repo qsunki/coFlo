b9f4cf4f62fc826385b70c3d73c6d28ee0ff4360
손지민 <jimmi219@naver.com>
2024-10-28 17:05:36 +0900

    feat: QueryDSLConfig 추가
    Jira: S11P31A210-313

--- backend/src/main/java/com/reviewping/coflo/global/config/QueryDSLConfig.java
+++ backend/src/main/java/com/reviewping/coflo/global/config/QueryDSLConfig.java
@@ -2,14 +2,13 @@ package com.reviewping.coflo.global.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class QueryDSLConfig {
-    private final EntityManager entityManager;
+    @PersistenceContext private EntityManager entityManager;
 
     @Bean
     public JPAQueryFactory jpaQueryFactory() {
