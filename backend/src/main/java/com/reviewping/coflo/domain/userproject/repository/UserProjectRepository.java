b294cfe25f542bec06bb972380e3e833fb8c1336
손지민 <jimmi219@naver.com>
2024-10-29 11:14:54 +0900

    feat: 프로젝트 연동 해제 API 구현
    Jira: S11P31A210-342

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
@@ -1,6 +1,12 @@
 package com.reviewping.coflo.domain.userproject.repository;
 
+import static com.reviewping.coflo.global.error.ErrorCode.USER_PROJECT_NOT_EXIST;
+
+import com.reviewping.coflo.domain.project.entity.Project;
+import com.reviewping.coflo.domain.user.entity.GitlabAccount;
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
+import com.reviewping.coflo.global.error.exception.BusinessException;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProjectRepository
@@ -8,4 +14,12 @@ public interface UserProjectRepository
     Boolean existsByGitlabAccountIdAndProjectId(Long gitlabAccountId, Long projectId);
 
     Boolean existsByGitlabAccountUserId(Long userId);
+
+    Optional<UserProject> findByProjectAndGitlabAccount(
+            Project project, GitlabAccount gitlabAccount);
+
+    default UserProject getByProjectAndGitlabAccount(Project project, GitlabAccount gitlabAccount) {
+        return findByProjectAndGitlabAccount(project, gitlabAccount)
+                .orElseThrow(() -> new BusinessException(USER_PROJECT_NOT_EXIST));
+    }
 }
