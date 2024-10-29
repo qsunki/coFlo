96398bfe4e452444eedb948ddecc4fc23a73180c
이보연 <btothey99@ewhain.net>
2024-10-29 10:47:13 +0900

    style: MR 타입 변경

--- frontend/src/types/mergeRequest.ts
+++ frontend/src/types/mergeRequest.ts
@@ -1,10 +1,40 @@
-export interface MergeRequest {
+export interface Assignee {
+  username: string;
+  avatar_url: string;
+}
+
+export interface Reviewer {
+  username: string;
+  avatar_url: string;
+}
+
+export interface GitlabMergeRequest {
   id: number;
-  branchName: string;
+  iid: number;
   title: string;
-  assignee: string;
-  reviewer: string;
-  createdAt: string;
+  description: string;
+  state: 'opened' | 'closed' | 'merged';
+  merged_at: string | null;
+  created_at: string;
+  updated_at: string;
+  closed_at: string | null;
+  source_branch: string;
+  target_branch: string;
   labels: string[];
-  author: string;
+  has_conflicts: boolean;
+  assignee: Assignee;
+  reviewer: Reviewer;
+  isAiReviewCreated: boolean;
+}
+
+export interface GitlabMrListResponse {
+  gitlabMrList: GitlabMergeRequest[];
+  totalPages: number;
+  totalElements: number;
+  isLast: boolean;
+  currPage: number;
+}
+
+export interface MrItemProps {
+  mergeRequest: GitlabMergeRequest;
 }
