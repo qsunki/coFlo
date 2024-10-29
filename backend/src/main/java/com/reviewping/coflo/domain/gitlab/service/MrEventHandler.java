95ac78a974190a46d59bfb20e9aed357ca9bb787
qsunki <ajsthfldu@outlook.com>
2024-10-29 12:26:26 +0900

    feat: Gitlab mr 웹훅 핸드러 구현
    Jira: S11P31A210-271

---
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/service/MrEventHandler.java
@@ -0,0 +1,81 @@
+package com.reviewping.coflo.domain.gitlab.service;
+
+import com.reviewping.coflo.domain.gitlab.dto.request.GitlabEventRequest;
+import com.reviewping.coflo.domain.project.entity.Project;
+import com.reviewping.coflo.domain.project.repository.ProjectRepository;
+import com.reviewping.coflo.domain.review.service.ReviewCreateService;
+import com.reviewping.coflo.global.error.ErrorCode;
+import com.reviewping.coflo.global.error.exception.BusinessException;
+import jakarta.annotation.PostConstruct;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MrEventHandler {
+
+    private final Map<String, BiConsumer<Long, GitlabEventRequest>> handlers = new HashMap<>();
+    private final ReviewCreateService reviewCreateService;
+    private final ProjectRepository projectRepository;
+
+    @PostConstruct
+    void initHandlers() {
+        handlers.put("open", this::handleOpen);
+    }
+
+    @Async
+    public void handle(Long projectId, GitlabEventRequest gitlabEventRequest) {
+        handlers.getOrDefault(
+                        gitlabEventRequest.objectAttributes().action(),
+                        (id, request) -> {
+                            throw new BusinessException(ErrorCode.UNSUPPORTED_WEBHOOK_ACTION);
+                        })
+                .accept(projectId, gitlabEventRequest);
+    }
+
+    private void handleOpen(Long projectId, GitlabEventRequest gitlabEventRequest) {
+        // 1. 필요한 정보 추출
+        String gitlabUrl = getGitlabUrl(gitlabEventRequest);
+        Long gitlabProjectId = getGitlabProjectId(gitlabEventRequest);
+        Long iid = getIid(gitlabEventRequest);
+        String mrDescription = getMrDescription(gitlabEventRequest);
+        // 2. gitlab project token 찾기
+        Project project = projectRepository.getById(projectId);
+        String token = project.getBotToken();
+        // 3. 리뷰 달기
+        reviewCreateService.makeCodeReviewWhenCalledByWebhook(
+                gitlabUrl, token, gitlabProjectId, iid, mrDescription, projectId);
+    }
+
+    private String getMrDescription(GitlabEventRequest gitlabEventRequest) {
+        return gitlabEventRequest.objectAttributes().description();
+    }
+
+    private Long getIid(GitlabEventRequest gitlabEventRequest) {
+        return gitlabEventRequest.objectAttributes().iid();
+    }
+
+    private Long getGitlabProjectId(GitlabEventRequest gitlabEventRequest) {
+        return gitlabEventRequest.project().id();
+    }
+
+    private String getGitlabUrl(GitlabEventRequest gitlabEventRequest) {
+        try {
+            URI uri = new URI(gitlabEventRequest.project().webUrl());
+            URL parsedUrl = uri.toURL();
+            return parsedUrl.getHost();
+        } catch (IllegalArgumentException | URISyntaxException | MalformedURLException e) {
+            throw new BusinessException(ErrorCode.GITLAB_URL_PARSE_ERROR);
+        }
+    }
+}
