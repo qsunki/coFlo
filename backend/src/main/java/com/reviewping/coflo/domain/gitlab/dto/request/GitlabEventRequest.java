d9c79c6ca2e932d85449667e2493d233e0ac2b2b
qsunki <ajsthfldu@outlook.com>
2024-10-29 11:17:20 +0900

    feat: Gitlab mr event 수신용 dto 생성
    Jira: S11P31A210-271

---
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/dto/request/GitlabEventRequest.java
@@ -0,0 +1,22 @@
+package com.reviewping.coflo.domain.gitlab.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record GitlabEventRequest(
+        String objectKind, String eventType, Project project, ObjectAttributes objectAttributes) {
+
+    public record Project(Long id, String webUrl) {}
+
+    public record ObjectAttributes(
+            Long id,
+            Long iid,
+            String title,
+            String createdAt,
+            String updatedAt,
+            String lastEditedAt,
+            Boolean draft,
+            String description,
+            String action) {}
+}
