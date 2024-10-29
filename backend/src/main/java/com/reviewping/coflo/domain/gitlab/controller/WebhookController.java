6ccd935ff81527c085f75f0da2d9fd4e2b714ea3
qsunki <ajsthfldu@outlook.com>
2024-10-29 12:26:44 +0900

    feat: Gitlab mr 웹훅 컨트롤러 구현
    Jira: S11P31A210-271

---
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/controller/WebhookController.java
@@ -0,0 +1,39 @@
+package com.reviewping.coflo.domain.gitlab.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reviewping.coflo.domain.gitlab.dto.request.GitlabEventRequest;
+import com.reviewping.coflo.domain.gitlab.service.MrEventHandler;
+import com.reviewping.coflo.global.common.response.ApiResponse;
+import com.reviewping.coflo.global.common.response.impl.ApiSuccessResponse;
+import com.reviewping.coflo.global.error.ErrorCode;
+import com.reviewping.coflo.global.error.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class WebhookController {
+
+    private final MrEventHandler mrEventHandler;
+    private final ObjectMapper objectMapper;
+
+    @PostMapping("/webhook/{projectId}")
+    public ApiResponse<Void> handleGitlabEvent(
+            @RequestHeader("X-Gitlab-Event") String gitlabEventType,
+            @PathVariable("projectId") Long projectId,
+            @RequestBody String requestBody) {
+        if (gitlabEventType.equals("Merge Request Hook")) {
+            GitlabEventRequest gitlabEventRequest;
+            try {
+                gitlabEventRequest = objectMapper.readValue(requestBody, new TypeReference<>() {});
+            } catch (JsonProcessingException e) {
+                throw new BusinessException(ErrorCode.GITLAB_EVENT_REQUEST_SERIALIZATION_ERROR);
+            }
+            mrEventHandler.handle(projectId, gitlabEventRequest);
+            return ApiSuccessResponse.success();
+        }
+        throw new BusinessException(ErrorCode.UNSUPPORTED_EVENT_TYPE);
+    }
+}
