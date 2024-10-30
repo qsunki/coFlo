ca61c0de1d73dcaf404256a8c754f662d5559192
손지민 <jimmi219@naver.com>
2024-10-30 10:51:26 +0900

    feat: 현재 기준 프로젝트 주차와 시작일과 종료일 계산 구현
    Jira: S11P31A210-329

---
+++ backend/src/main/java/com/reviewping/coflo/domain/project/controller/response/ProjectTeamRewardResponse.java
@@ -0,0 +1,18 @@
+package com.reviewping.coflo.domain.project.controller.response;
+
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Builder;
+
+@Builder
+public record ProjectTeamRewardResponse(
+        LocalDate startDate, LocalDate endDate, List<UserScoreInfoResponse> userScores) {
+    public static ProjectTeamRewardResponse of(
+            LocalDate[] startAndEndDates, List<UserScoreInfoResponse> userScores) {
+        return ProjectTeamRewardResponse.builder()
+                .startDate(startAndEndDates[0])
+                .endDate(startAndEndDates[1])
+                .userScores(userScores)
+                .build();
+    }
+}
--- backend/src/main/java/com/reviewping/coflo/domain/project/service/ProjectStatisticsService.java
+++ backend/src/main/java/com/reviewping/coflo/domain/project/service/ProjectStatisticsService.java
@@ -1,6 +1,7 @@
 package com.reviewping.coflo.domain.project.service;
 
 import com.reviewping.coflo.domain.project.controller.response.ProjectTeamDetailResponse;
+import com.reviewping.coflo.domain.project.controller.response.ProjectTeamRewardResponse;
 import com.reviewping.coflo.domain.project.entity.Project;
 import com.reviewping.coflo.domain.project.repository.ProjectRepository;
 import com.reviewping.coflo.domain.user.entity.GitlabAccount;
@@ -8,6 +9,8 @@ import com.reviewping.coflo.domain.user.entity.User;
 import com.reviewping.coflo.domain.user.repository.GitlabAccountRepository;
 import com.reviewping.coflo.global.client.gitlab.GitLabClient;
 import com.reviewping.coflo.global.client.gitlab.response.ProjectInfoContent;
+import com.reviewping.coflo.global.util.ProjectDateUtil;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,5 +34,19 @@ public class ProjectStatisticsService {
                         project.getGitlabProjectId());
         Long aiReviewCount = projectRepository.findReviewCountByProjectId(project.getId());
         return ProjectTeamDetailResponse.of(projectInfoContent, aiReviewCount);
+    }
+
+    public ProjectTeamRewardResponse getTeamReward(Long projectId) {
+        Project project = projectRepository.getById(projectId);
+        LocalDate projectCreatedDate = project.getCreatedDate().toLocalDate();
+        int previousWeek = getPreviousWeek(projectCreatedDate);
+        LocalDate[] startAndEndDates =
+                ProjectDateUtil.calculateWeekStartAndEndDates(projectCreatedDate, previousWeek);
+        return ProjectTeamRewardResponse.builder().build();
+    }
+
+    private static int getPreviousWeek(LocalDate projectCreatedDate) {
+        int currentWeek = ProjectDateUtil.calculateWeekNumber(projectCreatedDate, LocalDate.now());
+        return Math.max(1, currentWeek - 1);
     }
 }
