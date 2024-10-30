93f68cff69eca27232c5c9ff89838fd2f4584574
손지민 <jimmi219@naver.com>
2024-10-30 11:59:18 +0900

    feat: 대표 뱃지 조회 기능 구현
    Jira: S11P31A210-329

--- backend/src/main/java/com/reviewping/coflo/domain/badge/repository/UserBadgeRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/badge/repository/UserBadgeRepository.java
@@ -1,6 +1,14 @@
 package com.reviewping.coflo.domain.badge.repository;
 
 import com.reviewping.coflo.domain.badge.entity.UserBadge;
+import com.reviewping.coflo.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {}
+public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
+
+    @Query("SELECT ub FROM UserBadge ub WHERE ub.user = :user AND ub.isSelected = true")
+    Optional<UserBadge> findSelectedBadgeByUser(@Param("user") User user);
+}
