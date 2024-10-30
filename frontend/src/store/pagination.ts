e44f6268c52b64d04e9e4cdf803221470614df05
anjs124 <87066209+anjs124@users.noreply.github.com>
2024-10-30 15:03:49 +0900

    fix: 패키지 변경
    Jira: S11P31A210-349

--- frontend/src/store/pagination.ts
+++ frontend/src/store/pagination.ts
@@ -1,9 +1,9 @@
-import { atom } from 'jotai';
+import { atom, Setter } from 'jotai';
 
 export const currentPageAtom = atom(1);
 export const totalPagesAtom = atom(1);
 
-export const updatePagination = (set: any, totalPages: number, currentPage: number) => {
+export const updatePagination = (set: Setter, totalPages: number, currentPage: number) => {
   set(totalPagesAtom, totalPages);
   set(currentPageAtom, currentPage);
 };
