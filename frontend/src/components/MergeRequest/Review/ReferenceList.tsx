67b1d6a51f4c74298956721b28a1431dd8bba065
이보연 <btothey99@ewhain.net>
2024-10-30 12:36:29 +0900

    feat: 참고자료 리스트 구현
    Jira: S11P31A210-282

---
+++ frontend/src/components/MergeRequest/Review/ReferenceList.tsx
@@ -0,0 +1,44 @@
+// components/references/ReferencesList.tsx
+import { Link } from 'react-router-dom';
+import { Reference } from 'types/review';
+import ReviewComment from 'components/MergeRequest/Review/ReviewComment';
+
+interface ReferencesListProps {
+  references: Reference[];
+}
+
+const ReferencesList = ({ references }: ReferencesListProps) => {
+  return (
+    <div className="p-4 font-pretendard w-[500px]">
+      {/* Header */}
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold">References</h2>
+        <Link to="/references" className=" hover:text-gray-800 transition-colors">
+          전체보기
+        </Link>
+      </div>
+
+      {/* References List */}
+      <div className="space-y-4">
+        {references.map((reference) => (
+          <div
+            key={reference.id}
+            className={`
+            ${reference.type === 'code' ? 'max-h-[330px]' : 'max-h-[300px]'}
+            rounded-lg
+            border-2 border-secondary
+          `}
+          >
+            <ReviewComment
+              title={reference.fileName}
+              content={reference.content}
+              type={reference.type}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ReferencesList;
