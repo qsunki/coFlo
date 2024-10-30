9f4d870a15b7696cd9875eabc12e2f44cd2a6fb9
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-30 14:18:14 +0900

    feat: 나의 뱃지 목록 상세 조회
    Jira: S11P31A210-360

---
+++ backend/src/main/java/com/reviewping/coflo/domain/badge/controller/BadgeController.java
@@ -0,0 +1,25 @@
+package com.reviewping.coflo.domain.badge.controller;
+
+import com.reviewping.coflo.domain.badge.controller.dto.response.BadgeResponse;
+import com.reviewping.coflo.domain.badge.service.BadgeService;
+import com.reviewping.coflo.domain.user.entity.User;
+import com.reviewping.coflo.global.auth.AuthUser;
+import com.reviewping.coflo.global.common.response.ApiResponse;
+import com.reviewping.coflo.global.common.response.impl.ApiSuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/badges")
+@RequiredArgsConstructor
+public class BadgeController {
+
+    private final BadgeService badgeService;
+
+    @GetMapping
+    public ApiResponse<BadgeResponse> getBadgeInfo(@AuthUser User user) {
+        return ApiSuccessResponse.success(badgeService.getBadgeInfo(user));
+    }
+}
--- backend/src/main/java/com/reviewping/coflo/domain/badge/repository/UserBadgeRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/badge/repository/UserBadgeRepository.java
@@ -2,7 +2,9 @@ package com.reviewping.coflo.domain.badge.repository;
 
 import com.reviewping.coflo.domain.badge.entity.UserBadge;
 import com.reviewping.coflo.domain.user.entity.User;
+import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,7 @@ public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
 
     @Query("SELECT ub FROM UserBadge ub WHERE ub.user = :user AND ub.isSelected = true")
     Optional<UserBadge> findSelectedBadgeByUser(@Param("user") User user);
+
+    @EntityGraph(attributePaths = {"badge"})
+    List<UserBadge> findAllByUser(User user);
 }
