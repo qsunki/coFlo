3d356200307b409cc881b7b05a5a5a3f464354c2
이보연 <btothey99@ewhain.net>
2024-10-30 12:37:41 +0900

    feat: 리뷰 코멘트 참고자료에도 적용할 수 있도록 수정
    Jira: S11P31A210-282

--- frontend/src/components/MergeRequest/Review/ReviewComment.tsx
+++ frontend/src/components/MergeRequest/Review/ReviewComment.tsx
@@ -3,21 +3,33 @@ import { ReviewCommentProps } from 'types/review.ts';
 
 export default function ReviewComment({
   name,
+  title,
   content,
+  type,
   createdAt,
   reviewer,
   backgroundColor = 'bg-white',
 }: ReviewCommentProps) {
   return (
-    <div>
+    <div className="flex flex-col">
       <div
-        className={`flex items-center gap-2 p-2 border-b-2 border-secondary rounded-t-md ${backgroundColor}`}
+        className={`flex items-center gap-2 p-2 border-b-2 border-secondary rounded-t-md ${backgroundColor} flex-shrink-0`}
       >
-        <span className="font-bold">{name}</span>
-        <span className="text-gray-700">left a comment</span>
+        {name && (
+          <>
+            <span className="font-bold">{name}</span>
+            <span className="text-gray-700">left a comment</span>
+          </>
+        )}
+
+        {title && (
+          <span className={`${type === 'code' ? 'font-SFMono text-sm' : ''} font-bold`}>
+            {title}
+          </span>
+        )}
       </div>
       {/* 코멘트 내용 */}
-      <div className="p-4">
+      <div className={` p-4 ${type === 'code' ? 'bg-secondary/30 font-SFMono text-sm' : ''}`}>
         {reviewer && (
           <div className="flex items-center gap-2 mb-2">
             <img src={reviewer.avatarUrl} alt={reviewer.name} className="w-6 h-6 rounded-full" />
@@ -27,7 +39,18 @@ export default function ReviewComment({
             </span>
           </div>
         )}
-        <ReactMarkdown className="prose max-w-none whitespace-pre-wrap">{content}</ReactMarkdown>
+
+        {type ? (
+          <div
+            className={`${type === 'code' ? 'overflow-auto max-h-[220px]' : 'overflow-y-auto max-h-[220px]'}`}
+          >
+            <ReactMarkdown className={`${type ? '' : 'prose'} max-w-none whitespace-pre-wrap`}>
+              {content}
+            </ReactMarkdown>
+          </div>
+        ) : (
+          <ReactMarkdown className="prose max-w-none whitespace-pre-wrap">{content}</ReactMarkdown>
+        )}
       </div>
     </div>
   );
