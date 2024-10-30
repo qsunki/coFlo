f6ee007570d5bebe682d207f895cafc5f821f158
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-10-30 20:17:37 +0900

    refactor: 변수명 변경
    Jira: S11P31A210-361

--- backend/src/main/java/com/reviewping/coflo/domain/badge/controller/dto/response/BadgeDetail.java
+++ backend/src/main/java/com/reviewping/coflo/domain/badge/controller/dto/response/BadgeDetail.java
@@ -2,9 +2,9 @@ package com.reviewping.coflo.domain.badge.controller.dto.response;
 
 import com.reviewping.coflo.domain.badge.entity.Badge;
 
-public record BadgeDetail(Long badgeId, String name, String description, String imageUrl) {
-    public static BadgeDetail of(Badge badge) {
+public record BadgeDetail(Long userBadgeId, String name, String description, String imageUrl) {
+    public static BadgeDetail of(Long userBadgeId, Badge badge) {
         return new BadgeDetail(
-                badge.getId(), badge.getName(), badge.getDescription(), badge.getImageUrl());
+                userBadgeId, badge.getName(), badge.getDescription(), badge.getImageUrl());
     }
 }
