6eb04b4a816b6fac6c2b33fd21e02482b7e6191a
anjs124 <87066209+anjs124@users.noreply.github.com>
2024-10-30 13:30:28 +0900

    feat: 오타수정
    Jira: S11P31A210-362

--- frontend/src/components/Repository/RepositoryItem.tsx
+++ frontend/src/components/Repository/RepositoryItem.tsx
@@ -2,7 +2,7 @@ export function RepositoryItem({ name, integrate }: { name: string; integrate: s
   return (
     <div className=" pl-3 w-[800px]">
       <div className="flex flex-row justify-between ">
-        <h2 className="text-lx font-semibold">{name}</h2>
+        <h2 className="text-xl font-semibold">{name}</h2>
         <h2 className="text-xs font-semibold">{integrate}</h2>
       </div>
     </div>
