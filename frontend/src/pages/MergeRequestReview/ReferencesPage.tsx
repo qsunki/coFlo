6747f351d2974dc9ca4ccf4edbb5115d4abd08f8
이보연 <btothey99@ewhain.net>
2024-10-31 10:15:48 +0900

    feat: 레퍼런스 페이지 레이아웃 구현
    Jira: S11P31A210-283

---
+++ frontend/src/pages/MergeRequestReview/ReferencesPage.tsx
@@ -0,0 +1,32 @@
+import ReferenceHeader from '@components/MergeRequest/Reference/ReferenceHeader.tsx';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Reference } from 'types/review.ts';
+import ReferencesList from '@components/MergeRequest/Reference/ReferencesList.tsx';
+
+const ReferencesPage = () => {
+  const { id } = useParams();
+  const [references, setReferences] = useState<Reference[]>([]);
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchReferences = async () => {
+      const response = await fetch(`/api/reviews/${id}/retrivals`);
+      const data = await response.json();
+      setReferences(data);
+    };
+
+    fetchReferences();
+  }, [id]);
+
+  if (!references) return <div>Loading...</div>;
+
+  return (
+    <div className="flex flex-col p-10">
+      <ReferenceHeader />
+      <ReferencesList references={references} />
+    </div>
+  );
+};
+
+export default ReferencesPage;
