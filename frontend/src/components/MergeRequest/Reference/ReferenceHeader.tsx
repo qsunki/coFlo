f4baa70f6a212a1b20c2e32fb9894cbd0a3ac357
이보연 <btothey99@ewhain.net>
2024-10-31 10:15:00 +0900

    feat: 레퍼런스 페이지 헤더 구현
    Jira: S11P31A210-283

---
+++ frontend/src/components/MergeRequest/Reference/ReferenceHeader.tsx
@@ -0,0 +1,10 @@
+const ReferenceHeader = () => {
+  return (
+    <div className="flex flex-col space-y-2 mb-6">
+      <span className="font-bold text-3xl">References</span>
+      <span className="font-bold">내용을 수정하여 리뷰를 재생성 할 수 있습니다.</span>
+    </div>
+  );
+};
+
+export default ReferenceHeader;
