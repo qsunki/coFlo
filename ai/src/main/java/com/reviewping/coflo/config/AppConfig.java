64ea960d1ee83156c65e5f90a005325403c8767e
qsunki <ajsthfldu@outlook.com>
2024-10-31 13:59:51 +0900

    feat: TSparser 빈 설정
    Jira: S11P31A210-239

--- ai/src/main/java/com/reviewping/coflo/config/AppConfig.java
+++ ai/src/main/java/com/reviewping/coflo/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.reviewping.coflo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.treesitter.TSParser;
 
 @Configuration(proxyBeanMethods = false)
 public class AppConfig {
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public TSParser tsParser() {
+        return new TSParser();
     }
 }
