dd02622c5484ca34c7dfd705f1957507843e8235
유승현 <fview@naver.com>
2024-10-31 16:41:46 +0900

    feat: 베스트 mr 조회 repository query문 작성
    Jira: S11P31A210-344

--- backend/src/main/java/com/reviewping/coflo/domain/mergerequest/repository/MrInfoRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/mergerequest/repository/MrInfoRepository.java
@@ -3,8 +3,12 @@ package com.reviewping.coflo.domain.mergerequest.repository;
 import com.reviewping.coflo.domain.mergerequest.entity.MrInfo;
 import com.reviewping.coflo.global.error.ErrorCode;
 import com.reviewping.coflo.global.error.exception.BusinessException;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MrInfoRepository extends JpaRepository<MrInfo, Long> {
     Optional<MrInfo> findByProjectIdAndGitlabMrIid(Long projectId, Long iid);
@@ -15,4 +19,15 @@ public interface MrInfoRepository extends JpaRepository<MrInfo, Long> {
         return findByProjectIdAndGitlabMrIid(projectId, iid)
                 .orElseThrow(() -> new BusinessException(ErrorCode.MR_INFO_NOT_EXIST));
     }
+
+    @Query(
+            value =
+                    "SELECT * FROM mr_info m WHERE m.gitlab_created_date BETWEEN :startDate AND"
+                        + " :endDate ORDER BY (COALESCE(m.readability_score, 0) +"
+                        + " COALESCE(m.consistency_score, 0) + COALESCE(m.reusability_score, 0) +"
+                        + " COALESCE(m.reliability_score, 0) + COALESCE(m.security_score, 0) +"
+                        + " COALESCE(m.maintainability_score, 0)) DESC LIMIT 3",
+            nativeQuery = true)
+    List<MrInfo> findTop3MrInfoList(
+            @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 }
