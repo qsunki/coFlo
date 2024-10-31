5fa517020ea3c791980efad82f83b31a9f5e175b
이보연 <btothey99@ewhain.net>
2024-10-31 10:06:15 +0900

    feat: 레퍼런스 목록 구현
    Jira: S11P31A210-283

--- frontend/src/components/MergeRequest/Reference/CommonReference.tsx
+++ frontend/src/components/MergeRequest/Reference/CommonReference.tsx
@@ -3,14 +3,22 @@ import { useState } from 'react';
 import CodeReference from './CodeReference';
 import TextReference from './TextReference';
 import { Pencil, Trash2 } from 'lucide-react';
+import { CommonReferenceProps } from 'types/review.ts';
 
-const CommonReference = ({ id, title, content, type, onEdit, onDelete }: CommonReferenceProps) => {
+const CommonReference = ({
+  id,
+  fileName,
+  content,
+  type,
+  onEdit,
+  onDelete,
+}: CommonReferenceProps) => {
   const [isEditing, setIsEditing] = useState(false);
 
   return (
     <div className="rounded-lg border-2 border-secondary">
-      <div className="flex justify-between items-center p-4 bg-gray-50">
-        <h3 className="font-bold">{title}</h3>
+      <div className="flex justify-between items-center p-4 bg-white rounded-t-lg border-b-2 border-secondary">
+        <h3 className="font-bold">{fileName}</h3>
         <div className="flex gap-2">
           <button onClick={() => setIsEditing(!isEditing)}>
             <Pencil className="w-4 h-4" />
