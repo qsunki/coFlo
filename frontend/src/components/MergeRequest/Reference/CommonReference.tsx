bc4d8745aec0133bd39d49e5a5686dcd7e0e4c55
이보연 <btothey99@ewhain.net>
2024-10-31 16:58:56 +0900

    feat: 수정, 삭제 기능 수정
    Jira: S11P31A210-283

--- frontend/src/components/MergeRequest/Reference/CommonReference.tsx
+++ frontend/src/components/MergeRequest/Reference/CommonReference.tsx
@@ -5,50 +5,103 @@ import TextReference from './TextReference';
 import { CommonReferenceProps } from 'types/review.ts';
 import { PencilIcon } from '@components/TextDiv/Icons/PencilIcon';
 import { TrashIcon } from '@components/TextDiv/Icons/TrashIcon';
+import { CheckIcon, ArrowDownToLine, Undo2 } from 'lucide-react';
 
 const CommonReference = ({
   id,
   fileName,
   content,
   type,
+  language,
   onEdit,
   onDelete,
 }: CommonReferenceProps) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState(content);
+  const [showSaveMessage, setShowSaveMessage] = useState(false);
+
+  const handleEdit = (newContent: string) => {
+    setEditedContent(newContent);
+  };
+
+  const handleSave = () => {
+    onEdit(id, editedContent);
+    setShowSaveMessage(true);
+    setTimeout(() => {
+      setShowSaveMessage(false);
+      setIsEditing(false);
+    }, 1000);
+  };
+
+  const handleCancel = () => {
+    setEditedContent(content);
+    setIsEditing(false);
+  };
 
   return (
-    <div className="rounded-lg border-2 border-secondary">
-      <div className="flex justify-between items-center p-2 bg-white rounded-t-lg border-b-2 border-secondary">
+    <div className="rounded-lg border-2 border-primary-500">
+      <div className="flex justify-between items-center p-2 bg-white rounded-t-lg border-b-2 border-primary-500">
         <h3 className={`font-bold ${type === 'CODE' ? 'font-SFMono text-sm' : ''}`}>{fileName}</h3>
         <div className="flex gap-2">
-          <button onClick={() => setIsEditing(!isEditing)}>
-            <PencilIcon className="w-4 h-4 text-primary-500" />
-          </button>
-          <button onClick={() => onDelete(id)}>
-            <TrashIcon className="w-4 h-4 text-primary-500" />
-          </button>
+          {isEditing ? (
+            <>
+              <button
+                onClick={handleSave}
+                className="flex items-center gap-2 text-primary-500 hover:text-secondary"
+                disabled={showSaveMessage}
+              >
+                {showSaveMessage ? (
+                  <>
+                    <CheckIcon className="w-4 h-4" strokeWidth={3} />
+                    <span className="text-sm animate-fade-out">저장되었습니다</span>
+                  </>
+                ) : (
+                  <ArrowDownToLine className="w-4 h-4" strokeWidth={3} />
+                )}
+              </button>
+              <button
+                onClick={handleCancel}
+                className="flex items-center gap-1 text-primary-500 hover:text-secondary"
+                disabled={showSaveMessage}
+              >
+                <Undo2 className="w-4 h-4" strokeWidth={3} />
+              </button>
+            </>
+          ) : (
+            <>
+              <button onClick={() => setIsEditing(true)}>
+                <PencilIcon className="w-4 h-4 text-primary-500 hover:text-secondary" />
+              </button>
+              <button onClick={() => onDelete(id)} className="">
+                <TrashIcon className="w-4 h-4 text-primary-500 hover:text-secondary" />
+              </button>
+            </>
+          )}
         </div>
       </div>
-      <div className="">
+      <div>
         {isEditing ? (
           type === 'CODE' ? (
             <CodeReference
               id={id}
-              content={content}
-              onEdit={onEdit}
-              onCancel={() => setIsEditing(false)}
+              content={editedContent}
+              onEdit={handleEdit}
+              onCancel={handleCancel}
+              language={language}
             />
           ) : (
             <TextReference
               id={id}
-              content={content}
-              onEdit={onEdit}
-              onCancel={() => setIsEditing(false)}
+              content={editedContent}
+              onEdit={handleEdit}
+              onCancel={handleCancel}
             />
           )
         ) : (
-          <div className={`${type === 'CODE' ? 'font-SFMono bg-secondary/30 ' : ''} p-4`}>
-            {content}
+          <div
+            className={`${type === 'CODE' ? 'font-SFMono bg-secondary/30' : ''} p-4 whitespace-break-spaces max-h-[420px] overflow-y-auto`}
+          >
+            {editedContent}
           </div>
         )}
       </div>
