78874ed524c80ca804ea0ac3b75849d35a2d190a
이보연 <btothey99@ewhain.net>
2024-10-31 17:23:12 +0900

    style: 레이아웃 수정
    Jira: S11P31A210-283

--- frontend/src/components/MergeRequest/Reference/ReferenceHeader.tsx
+++ frontend/src/components/MergeRequest/Reference/ReferenceHeader.tsx
@@ -1,6 +1,6 @@
 const ReferenceHeader = () => {
   return (
-    <div className="flex flex-col space-y-2 mb-6">
+    <div className="flex flex-col space-y-2 mb-6 min-w-[350px]">
       <span className="font-bold text-3xl">References</span>
       <span className="font-bold">내용을 수정하여 리뷰를 재생성 할 수 있습니다.</span>
     </div>
