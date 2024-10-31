124e3bae7d80163fd33ac04a2a596c244a25e6fd
손지민 <jimmi219@naver.com>
2024-10-31 13:45:04 +0900

    feat: LanguageCode 데이터 삽입
    Jira: S11P31A210-378

---
+++ backend/src/main/java/com/reviewping/coflo/domain/project/repository/LanguageCodeRepository.java
@@ -0,0 +1,6 @@
+package com.reviewping.coflo.domain.project.repository;
+
+import com.reviewping.coflo.domain.project.entity.LanguageCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageCodeRepository extends JpaRepository<LanguageCode, Long> {}
