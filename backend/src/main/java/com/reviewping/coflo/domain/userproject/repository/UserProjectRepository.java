1a4730492e12339a09970df2a4b86a598090261f
손지민 <jimmi219@naver.com>
2024-11-01 12:45:08 +0900

    refactor: 불필요한 코드 삭제
    Jira: S11P31A210-380

--- backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/userproject/repository/UserProjectRepository.java
@@ -6,11 +6,8 @@ import com.reviewping.coflo.domain.project.entity.Project;
 import com.reviewping.coflo.domain.user.entity.GitlabAccount;
 import com.reviewping.coflo.domain.userproject.entity.UserProject;
 import com.reviewping.coflo.global.error.exception.BusinessException;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserProjectRepository
         extends JpaRepository<UserProject, Long>, UserProjectRepositoryCustom {
@@ -21,31 +18,8 @@ public interface UserProjectRepository
     Optional<UserProject> findByProjectAndGitlabAccount(
             Project project, GitlabAccount gitlabAccount);
 
-    List<UserProject> findByProject(Project project);
-
     default UserProject getByProjectAndGitlabAccount(Project project, GitlabAccount gitlabAccount) {
         return findByProjectAndGitlabAccount(project, gitlabAccount)
                 .orElseThrow(() -> new BusinessException(USER_PROJECT_NOT_EXIST));
     }
-
-    @Query(
-            value =
-                    """
-                            SELECT up.*
-                            FROM user_project_score ups
-                            JOIN user_project up ON ups.user_project_id = up.id
-                            JOIN gitlab_account ga ON up.gitlab_account_id = ga.id
-                            JOIN "user" u ON ga.user_id = u.id
-                            WHERE up.project_id = :projectId
-                            AND ups.week = :week
-                            AND u.id != :userId
-                            GROUP BY up.id
-                            ORDER BY SUM(ups.total_score) DESC
-                            LIMIT 5
-                            """,
-            nativeQuery = true)
-    List<UserProject> findTopScoreUserProjectsOfWeek(
-            @Param("projectId") Long projectId,
-            @Param("week") int week,
-            @Param("userId") Long userId);
 }
