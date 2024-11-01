8695bb2aeebb9f15611858b2c7d394298b7dae3d
유승현 <fview@naver.com>
2024-11-01 10:07:47 +0900

    feat: language type enum 생성
    Jira: S11P31A210-345

---
+++ backend/src/main/java/com/reviewping/coflo/domain/review/entity/LanguageType.java
@@ -0,0 +1,29 @@
+package com.reviewping.coflo.domain.review.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum LanguageType {
+    TYPESCRIPT("typescript"),
+    JAVASCRIPT("javascript"),
+    PYTHON("python"),
+    JAVA("java"),
+    C("c"),
+    CPP("cpp"),
+    CSHARP("csharp"),
+    GO("go"),
+    RUBY("ruby"),
+    RUST("rust"),
+    SWIFT("swift"),
+    PHP("php"),
+    SHELL("shell"),
+    SQL("sql"),
+    HTML("html"),
+    CSS("css");
+
+    private final String type;
+
+    LanguageType(String type) {
+        this.type = type;
+    }
+}
