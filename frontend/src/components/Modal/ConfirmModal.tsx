09a9721693b0f27f9c7f0c12be9440e1466c5570
이보연 <btothey99@ewhain.net>
2024-11-01 16:37:55 +0900

    feat: 경고 모달, 확인 모달 구현
    Jira: S11P31A210-283

--- frontend/src/components/Modal/ConfirmModal.tsx
+++ frontend/src/components/Modal/ConfirmModal.tsx
@@ -1,18 +1,22 @@
-import { AlertModalProps } from 'types/modal.ts';
-import { TriangleAlert } from 'lucide-react';
+import { ConfirmModalProps } from 'types/modal.ts';
 
-const AlertModal = ({ content, onConfirm, className = '' }: AlertModalProps) => {
+const ConfirmModal = ({ content, onConfirm, onCancel, className = '' }: ConfirmModalProps) => {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="fixed inset-0 bg-black opacity-50"></div>
       <div
         className={`flex flex-col items-center justify-center bg-white rounded-lg p-6 max-w-sm mx-4 relative z-10 border-2 border-primary-500 ${className}`}
       >
-        <TriangleAlert className="text-primary-500 my-4" size={48} />
         {content.map((text) => (
           <p className="text-center mb-6 text-xl text-primary-500">{text}</p>
         ))}
         <div className="flex justify-center gap-4">
+          <button
+            className="px-6 py-2 rounded-full border-2 border-primary-500 text-primary-500 bg-white"
+            onClick={() => onCancel()}
+          >
+            취소
+          </button>
           <button
             className="px-6 py-2 rounded-full border-2 border-primary-500 bg-primary-500 text-white"
             onClick={() => onConfirm()}
@@ -25,4 +29,4 @@ const AlertModal = ({ content, onConfirm, className = '' }: AlertModalProps) => 
   );
 };
 
-export default AlertModal;
+export default ConfirmModal;
