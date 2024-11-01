cf1228801163575fd3df6b9dfadb9ea15a197be3
유승현 <fview@naver.com>
2024-11-01 16:29:58 +0900

    feat: review repository getbyid default 메소드 추가
    Jira: S11P31A210-345

--- backend/src/main/java/com/reviewping/coflo/domain/review/repository/ReviewRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/review/repository/ReviewRepository.java
@@ -1,6 +1,14 @@
 package com.reviewping.coflo.domain.review.repository;
 
 import com.reviewping.coflo.domain.review.entity.Review;
+import com.reviewping.coflo.global.error.ErrorCode;
+import com.reviewping.coflo.global.error.exception.BusinessException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {}
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    default Review getById(Long reviewId) {
+        return findById(reviewId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.REVIEW_NOT_FOUND));
+    }
+}
