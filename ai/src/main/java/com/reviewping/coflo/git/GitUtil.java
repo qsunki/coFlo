3377d8d185ce0ec240f7e969eb945f6ef4446a65
qsunki <ajsthfldu@outlook.com>
2024-11-01 09:46:19 +0900

    fix: local git repository 존재 시 오류 수정
    Jira: S11P31A210-239

--- ai/src/main/java/com/reviewping/coflo/git/GitUtil.java
+++ ai/src/main/java/com/reviewping/coflo/git/GitUtil.java
@@ -3,6 +3,7 @@ package com.reviewping.coflo.git;
 import java.io.File;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,24 @@ public class GitUtil {
 
     /**
      * Git 저장소의 특정 브랜치의 최신상태만을 클론
-     * @param gitUrl Git 저장소 URL
-     * @param token Git 접근 토큰
-     * @param branch Git 클론 할 브랜치
+     *
+     * @param gitUrl    Git 저장소 URL
+     * @param token     Git 접근 토큰
+     * @param branch    Git 클론 할 브랜치
      * @param localPath 저장할 Git 로컬 저장소 위치
      * @throws GitUtilException Git 클론 중 발생할 수 있는 예외
      */
-    public void shallowCloneBranch(String gitUrl, String branch, String token, String localPath) {
+    public void shallowCloneOrPull(String gitUrl, String branch, String token, String localPath) {
+        File localDir = new File(localPath);
+
+        if (isDirectoryValid(localDir)) {
+            pullUpdates(localDir, branch, token);
+        } else {
+            cloneRepository(gitUrl, branch, token, localDir);
+        }
+    }
+
+    public void cloneRepository(String gitUrl, String branch, String token, File localDir) {
 
         try {
             // Git 클론 설정
@@ -26,7 +38,7 @@ public class GitUtil {
                     Git.cloneRepository()
                             .setURI(gitUrl)
                             .setBranch(branch)
-                            .setDirectory(new File(localPath))
+                            .setDirectory(localDir)
                             .setDepth(1)
                             .setCredentialsProvider(
                                     new UsernamePasswordCredentialsProvider("token", token));
@@ -37,5 +49,23 @@ public class GitUtil {
         } catch (GitAPIException e) {
             throw new GitUtilException("Git clone failed: " + e.getMessage());
         }
+    }
+
+    private void pullUpdates(File localDir, String branch, String token) {
+        try (Git git = Git.open(localDir)) {
+            PullCommand pullCommand =
+                    git.pull()
+                            .setCredentialsProvider(
+                                    new UsernamePasswordCredentialsProvider("token", token))
+                            .setRemote("origin")
+                            .setRemoteBranchName(branch);
+            pullCommand.call();
+        } catch (Exception e) {
+            throw new GitUtilException("Git pull failed: " + e.getMessage());
+        }
+    }
+
+    private boolean isDirectoryValid(File directory) {
+        return directory.exists() && directory.isDirectory() && directory.list().length > 0;
     }
 }
