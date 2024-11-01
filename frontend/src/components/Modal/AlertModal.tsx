4480d2a4d518e5a627031211e3e286a527939e6e
이보연 <btothey99@ewhain.net>
2024-11-01 15:15:15 +0900

    feat: 경고 모달 구현
    Jira: S11P31A210-283

---
+++ frontend/src/components/Modal/AlertModal.tsx
@@ -0,0 +1,37 @@
+interface AlertModalProps {
+  content: string[];
+  onConfirm: () => void;
+  onCancel: () => void;
+  className?: string;
+}
+
+const AlertModal = ({ content, onConfirm, onCancel, className = '' }: AlertModalProps) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="fixed inset-0 bg-black opacity-50"></div>
+      <div
+        className={`flex flex-col items-center justify-center bg-white rounded-lg p-6 max-w-sm mx-4 relative z-10 border-2 border-primary-500 ${className}`}
+      >
+        {content.map((text) => (
+          <p className="text-center mb-6 text-xl text-primary-500">{text}</p>
+        ))}
+        <div className="flex justify-center gap-4">
+          <button
+            className="px-6 py-2 rounded-full border-2 border-primary-500 text-primary-500 bg-white"
+            onClick={() => onCancel()}
+          >
+            취소
+          </button>
+          <button
+            className="px-6 py-2 rounded-full border-2 border-primary-500 bg-primary-500 text-white"
+            onClick={() => onConfirm()}
+          >
+            확인
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlertModal;
