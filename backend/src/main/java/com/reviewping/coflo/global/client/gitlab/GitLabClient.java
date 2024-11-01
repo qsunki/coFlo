496395b6f47b8305d20024c16afdc19b79d45927
유승현 <fview@naver.com>
2024-11-01 16:30:37 +0900

    feat: get single merge request 메소드 추가
    Jira: S11P31A210-345

--- backend/src/main/java/com/reviewping/coflo/global/client/gitlab/GitLabClient.java
+++ backend/src/main/java/com/reviewping/coflo/global/client/gitlab/GitLabClient.java
@@ -1,6 +1,6 @@
 package com.reviewping.coflo.global.client.gitlab;
 
-import static com.reviewping.coflo.global.error.ErrorCode.EXTERNAL_API_BAD_REQUEST;
+import static com.reviewping.coflo.global.error.ErrorCode.*;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -77,20 +77,29 @@ public class GitLabClient {
     public List<GitlabMrResponse> getTop3MrList(
             String gitlabUrl, String token, Long gitlabProjectId, List<MrInfo> mrInfoList) {
         List<GitlabMrResponse> top3MrList = new ArrayList<>();
-        HttpHeaders headers = makeGitlabHeaders(token);
         for (MrInfo mrInfo : mrInfoList) {
-            String url =
-                    GitLabApiUrlBuilder.createGetMergeRequestsUrl(
-                            gitlabUrl, gitlabProjectId, mrInfo.getGitlabMrIid());
-            ResponseEntity<GitlabMrDetailContent> response =
-                    RestTemplateUtils.sendGetRequest(
-                            url, headers, new ParameterizedTypeReference<>() {});
-            GitlabMrDetailContent gitlabMrDetailContent = response.getBody();
-            if (gitlabMrDetailContent != null) {
-                top3MrList.add(GitlabMrResponse.of(gitlabMrDetailContent, false));
-            }
+            GitlabMrResponse singleMergeRequest =
+                    getSingleMergeRequest(
+                            gitlabUrl, token, gitlabProjectId, mrInfo.getGitlabMrIid());
+            top3MrList.add(singleMergeRequest);
         }
         return top3MrList;
+    }
+
+    public GitlabMrResponse getSingleMergeRequest(
+            String gitlabUrl, String token, Long gitlabProjectId, Long mergeRequestIid) {
+        HttpHeaders headers = makeGitlabHeaders(token);
+        String url =
+                GitLabApiUrlBuilder.createGetMergeRequestsUrl(
+                        gitlabUrl, gitlabProjectId, mergeRequestIid);
+        ResponseEntity<GitlabMrDetailContent> response =
+                RestTemplateUtils.sendGetRequest(
+                        url, headers, new ParameterizedTypeReference<>() {});
+        GitlabMrDetailContent gitlabMrDetailContent = response.getBody();
+        if (gitlabMrDetailContent == null) {
+            throw new BusinessException(EXTERNAL_API_NOT_FOUND);
+        }
+        return GitlabMrResponse.of(gitlabMrDetailContent, false);
     }
 
     public GitlabProjectDetailContent getSingleProject(
