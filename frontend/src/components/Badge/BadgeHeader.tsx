12ebbb7ad970d831eaf512838eafacbb5fc9775b
이보연 <btothey99@ewhain.net>
2024-11-03 17:41:25 +0900

    feat: 마이 뱃지 헤더 구현

---
+++ frontend/src/components/Badge/BadgeHeader.tsx
@@ -0,0 +1,12 @@
+const BadgeHeader = () => {
+  return (
+    <div className="flex flex-col space-y-2 mb-6 min-w-[350px]">
+      <span className="font-bold text-3xl">My Badge</span>
+      <div className="flex flex-col">
+        <span className="font-bold">다른 사람에게 보이는 뱃지를 설정 할 수 있습니다.</span>
+      </div>
+    </div>
+  );
+};
+
+export default BadgeHeader;
