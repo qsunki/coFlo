05184c15d8dbcbf6dcd339374eb1fb892a7a2ae0
qsunki <ajsthfldu@outlook.com>
2024-11-04 01:46:24 +0900

    feat: Gitlab 웹훅 요청 dto 수정 및 패키지 이동
    Jira: S11P31A210-278

--- backend/src/main/java/com/reviewping/coflo/global/client/gitlab/request/GitlabEventRequest.java
+++ backend/src/main/java/com/reviewping/coflo/global/client/gitlab/request/GitlabEventRequest.java
@@ -1,8 +1,12 @@
-package com.reviewping.coflo.domain.gitlab.controller.dto.request;
+package com.reviewping.coflo.global.client.gitlab.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.OffsetDateTime;
 
+/**
+ * @see <a href="https://archives.docs.gitlab.com/17.0/ee/user/project/integrations/webhook_events.html#merge-request-events">merge-request-events</a>
+ */
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GitlabEventRequest(
         String objectKind, String eventType, Project project, ObjectAttributes objectAttributes) {
@@ -12,10 +16,10 @@ public record GitlabEventRequest(
     public record ObjectAttributes(
             Long id,
             Long iid,
+            String targetBranch,
             String title,
-            String createdAt,
-            String updatedAt,
-            String lastEditedAt,
+            OffsetDateTime createdAt,
+            OffsetDateTime updatedAt,
             Boolean draft,
             String description,
             String action) {}
