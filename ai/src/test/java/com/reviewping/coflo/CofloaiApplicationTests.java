7f99911fe06b9cf9e040e7b795074e450eb4bb35
qsunki <ajsthfldu@outlook.com>
2024-11-04 01:35:29 +0900

    test: contextLoads 테스트 비활성화
    Jira: S11P31A210-278

--- ai/src/test/java/com/reviewping/coflo/CofloaiApplicationTests.java
+++ ai/src/test/java/com/reviewping/coflo/CofloaiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.reviewping.coflo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("test용 redis 미구축")
 class CofloaiApplicationTests {
 
     @Test
