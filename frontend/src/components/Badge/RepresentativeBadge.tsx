546ecf4f3a75eee35f320fd407c407fb0dc077bc
이보연 <btothey99@ewhain.net>
2024-11-04 17:06:09 +0900

    feat: 대표 뱃지 컴포넌트 구현
    Jira: S11P31A210-389

--- frontend/src/components/Badge/RepresentativeBadge.tsx
+++ frontend/src/components/Badge/RepresentativeBadge.tsx
@@ -1,21 +1,25 @@
-interface RepresentativeBadgeProps {
-  selectedBadge: string | null;
-  onSave: () => void;
-}
+import { RepresentativeBadgeProps } from 'types/badge';
 
-const RepresentativeBadge = ({ selectedBadge, onSave }: RepresentativeBadgeProps) => {
+const RepresentativeBadge = ({ selectedBadge, onSave, onDefault }: RepresentativeBadgeProps) => {
   return (
-    <div className="my-8 flex flex-col items-center">
-      <div className="w-40 h-40 border-2 border-primary-500 rounded-lg flex items-center justify-center mb-4">
+    <div className="my-8 flex flex-col items-center ">
+      <div className="w-40 h-40 bg-primary-500 border-2 border-primary-500 rounded-lg flex items-center justify-center mb-4">
         {selectedBadge ? (
           <img src={selectedBadge} alt="대표 뱃지" className="w-32 h-32 object-contain" />
         ) : (
-          <p className="text-gray-400">대표 뱃지를 선택해주세요</p>
+          <div className="flex flex-col items-center text-gray-700">
+            <span>대표 뱃지를</span> <span> 선택해주세요</span>
+          </div>
         )}
       </div>
-      <button onClick={onSave} className="px-4 py-2 bg-primary-500 text-white rounded-lg">
-        저장
-      </button>
+      <div className="flex flex-row gap-4">
+        <button onClick={onDefault} className="px-4 py-2 bg-primary-500 text-white rounded-lg">
+          설정 안 함
+        </button>
+        <button onClick={onSave} className="px-4 py-2 bg-primary-500 text-white rounded-lg">
+          저장
+        </button>
+      </div>
     </div>
   );
 };
