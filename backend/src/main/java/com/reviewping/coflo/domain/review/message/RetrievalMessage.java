3929493a80c16414a0a5db7ff706c93893f61021
qsunki <ajsthfldu@outlook.com>
2024-11-04 14:15:15 +0900

    feat: language 정보 추가
    Jira: S11P31A210-461

--- backend/src/main/java/com/reviewping/coflo/domain/review/message/RetrievalMessage.java
+++ backend/src/main/java/com/reviewping/coflo/domain/review/message/RetrievalMessage.java
@@ -1,3 +1,3 @@
 package com.reviewping.coflo.domain.review.message;
 
-public record RetrievalMessage(String content, String fileName) {}
+public record RetrievalMessage(String content, String fileName, String language) {}
