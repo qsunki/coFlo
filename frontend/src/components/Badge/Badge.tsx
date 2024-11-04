c0ce2dbef9b4bd23ef292e5f35fd4de40a7bc89d
이보연 <btothey99@ewhain.net>
2024-11-04 17:42:37 +0900

    style: 뱃지 이미지 css 수정
    Jira: S11P31A210-389

--- frontend/src/components/Badge/Badge.tsx
+++ frontend/src/components/Badge/Badge.tsx
@@ -12,15 +12,15 @@ const Badge = ({
   },
 }: BadgeProps) => {
   const sizeClasses = {
-    sm: 'w-16 h-16',
-    md: 'w-24 h-24',
-    lg: 'w-32 h-32',
-    xl: 'w-40 h-40',
-    '2xl': 'w-48 h-48',
+    sm: 'min-w-[64px] min-h-[64px] w-16 h-16',
+    md: 'min-w-[96px] min-h-[96px] w-24 h-24',
+    lg: 'min-w-[128px] min-h-[128px] w-32 h-32',
+    xl: 'min-w-[160px] min-h-[160px] w-40 h-40',
+    '2xl': 'min-w-[192px] min-h-[192px] w-48 h-48',
   };
 
   return (
-    <div className="relative group">
+    <div className="relative group flex-shrink-0">
       <div
         onClick={onClick}
         className={`aspect-square flex items-center justify-center rounded-xl 
@@ -29,7 +29,7 @@ const Badge = ({
         <img
           src={badge.isAcquired ? badge.imageUrl : badge_none}
           alt={`뱃지 ${badge.badgeCodeId}`}
-          className={`${sizeClasses[size]} object-contain`}
+          className={`${sizeClasses[size]} object-contain flex-shrink-0`}
         />
       </div>
 
