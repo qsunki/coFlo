ff81ff7be9d7a76c0bc0313cb84f6927d71842e5
유승현 <fview@naver.com>
2024-11-04 17:37:35 +0900

    feat: branch repository 구현
    Jira: S11P31A210-411

---
+++ backend/src/main/java/com/reviewping/coflo/global/common/repository/BranchRepository.java
@@ -0,0 +1,16 @@
+package com.reviewping.coflo.global.common.repository;
+
+import com.reviewping.coflo.domain.project.entity.Branch;
+import com.reviewping.coflo.global.error.ErrorCode;
+import com.reviewping.coflo.global.error.exception.BusinessException;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch, Long> {
+    Optional<Branch> findByName(String name);
+
+    default Branch getByName(String name) {
+        return findByName(name)
+                .orElseThrow(() -> new BusinessException(ErrorCode.BRANCH_NOT_FOUND));
+    }
+}
