b8f00c0033906d7de2921742d2c7663573fa8c61
유승현 <fview@naver.com>
2024-11-04 17:38:37 +0900

    feat: retrieval repository 구현
    Jira: S11P31A210-411

---
+++ backend/src/main/java/com/reviewping/coflo/domain/review/repository/RetrievalRepository.java
@@ -0,0 +1,6 @@
+package com.reviewping.coflo.domain.review.repository;
+
+import com.reviewping.coflo.domain.review.entity.Retrieval;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RetrievalRepository extends JpaRepository<Retrieval, Long> {}
