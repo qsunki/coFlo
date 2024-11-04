c016430d597f7ddc14d20696b904befb2096c51e
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-11-04 12:55:25 +0900

    feat: RandomUtil 생성
    Jira: S11P31A210-440

---
+++ backend/src/main/java/com/reviewping/coflo/global/util/RandomUtil.java
@@ -0,0 +1,16 @@
+package com.reviewping.coflo.global.util;
+
+import java.util.Random;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomUtil {
+    // TODO: 운영시에 percent 바꾸기
+    private static final int percent = 100;
+    private Random random = new Random();
+
+    public boolean random() {
+        int value = random.nextInt(100);
+        return value < percent;
+    }
+}
