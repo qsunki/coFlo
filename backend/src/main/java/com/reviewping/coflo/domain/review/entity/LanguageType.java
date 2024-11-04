382a47bb193a62f61aea3296a4c0eaa13ab5c84d
유승현 <fview@naver.com>
2024-11-04 17:36:51 +0900

    feat: languagetype fromtype static 메소드 추가
    Jira: S11P31A210-411

--- backend/src/main/java/com/reviewping/coflo/domain/review/entity/LanguageType.java
+++ backend/src/main/java/com/reviewping/coflo/domain/review/entity/LanguageType.java
@@ -1,5 +1,7 @@
 package com.reviewping.coflo.domain.review.entity;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 
 @Getter
@@ -23,8 +25,23 @@ public enum LanguageType {
     PLAINTEXT("txt");
 
     private final String type;
+    private static final Map<String, LanguageType> LANGUAGE_TYPE_MAP = new HashMap<>();
+
+    static {
+        for (LanguageType lang : LanguageType.values()) {
+            LANGUAGE_TYPE_MAP.put(lang.type, lang);
+        }
+    }
 
     LanguageType(String type) {
         this.type = type;
+    }
+
+    public static LanguageType fromType(String type) {
+        LanguageType result = LANGUAGE_TYPE_MAP.get(type.toLowerCase());
+        if (result == null) {
+            throw new IllegalArgumentException("지원하지 않는 언어입니다: " + type);
+        }
+        return result;
     }
 }
