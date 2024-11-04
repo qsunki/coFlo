9a739fea63ec73f9016902db6fca1e8b2f60d72f
이보연 <btothey99@ewhain.net>
2024-11-04 14:21:53 +0900

    feat: 로그인, 회원가입 css 수정
    Jira: S11P31A210-389

--- frontend/src/components/Signup/SignupContent.tsx
+++ frontend/src/components/Signup/SignupContent.tsx
@@ -1,14 +1,11 @@
 import SignupForm from '@components/Signup/SignupForm.tsx';
+import signupBgImage from '@assets/images/signup_bg_image.png';
 
 const SignupContent = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
-      <div className="absolute inset-0 pointer-events-none">
-        <img
-          src="/images/signup_bg_image.webp"
-          alt="sign up"
-          className="w-full h-full object-cover"
-        />
+      <div className="absolute inset-0 pointer-events-none ">
+        <img src={signupBgImage} alt="sign up" className="w-full h-full object-cover" />
       </div>
       <div className="relative inset-x-10 mx-auto z-10 w-full max-w-lg ">
         <SignupForm />
