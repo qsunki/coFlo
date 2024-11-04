3b15aac2e8dcc2f5431d86bdb35229acef24de80
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-11-04 13:01:07 +0900

    refactor: static final 선언
    Jira: S11P31A210-440

--- backend/src/main/java/com/reviewping/coflo/global/util/RandomUtil.java
+++ backend/src/main/java/com/reviewping/coflo/global/util/RandomUtil.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class RandomUtil {
     // TODO: 운영시에 percent 바꾸기
     private static final int percent = 100;
-    private Random random = new Random();
+    private static final Random random = new Random();
 
     public boolean random() {
         int value = random.nextInt(100);
