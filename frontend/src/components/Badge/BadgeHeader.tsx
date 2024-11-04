ed32448a725fc8fd1085205068282440cc076b2a
이보연 <btothey99@ewhain.net>
2024-11-04 17:22:39 +0900

    style: 뱃지 페이지 레이아웃 수정
    Jira: S11P31A210-389

--- frontend/src/components/Badge/BadgeHeader.tsx
+++ frontend/src/components/Badge/BadgeHeader.tsx
@@ -1,6 +1,6 @@
 const BadgeHeader = () => {
   return (
-    <div className="flex flex-col space-y-2 mb-6 min-w-[350px]">
+    <div className="flex flex-col space-y-2 mb-6 w-full max-w-[1000px] min-w-[350px] items-start">
       <span className="font-bold text-3xl">My Badge</span>
       <div className="flex flex-col">
         <span className="font-bold">다른 사람에게 보이는 뱃지를 설정 할 수 있습니다.</span>
