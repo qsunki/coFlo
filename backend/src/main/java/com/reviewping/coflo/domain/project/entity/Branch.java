860b2cf72058ca97b8efa718162c584f6f07e0cf
손지민 <jimmi219@naver.com>
2024-11-04 13:10:05 +0900

    feat: Brach 엔티티 생성
    Jira: S11P31A210-453

---
+++ backend/src/main/java/com/reviewping/coflo/domain/project/entity/Branch.java
@@ -0,0 +1,31 @@
+package com.reviewping.coflo.domain.project.entity;
+
+import com.reviewping.coflo.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Entity
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class Branch extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    Project project;
+
+    String name;
+
+    @Builder
+    public Branch(Project project, String name) {
+        this.project = project;
+        this.name = name;
+        this.project.getBranches().add(this);
+    }
+}
