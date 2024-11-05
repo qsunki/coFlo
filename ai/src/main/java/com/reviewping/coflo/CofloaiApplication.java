d071f6ec817619e1593706eb3ceb8c4c291096a6
qsunki <ajsthfldu@outlook.com>
2024-11-05 16:40:30 +0900

    Revert "Merge branch 'ai/S11P31A210-391-ai서버-로깅-및-웹훅연결' into 'ai/dev'"
    This reverts merge request !120

--- ai/src/main/java/com/reviewping/coflo/CofloaiApplication.java
+++ ai/src/main/java/com/reviewping/coflo/CofloaiApplication.java
@@ -1,8 +1,10 @@
 package com.reviewping.coflo;
 
 import java.io.IOException;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 
 @SpringBootApplication
@@ -11,5 +13,13 @@ public class CofloaiApplication {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(CofloaiApplication.class, args);
+    }
+
+    // 애플리케이션 종료 방지
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+            Thread.currentThread().join();
+        };
     }
 }
