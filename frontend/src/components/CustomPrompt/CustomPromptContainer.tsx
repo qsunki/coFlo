eb87753ec20320c106a8911a36ade47872380553
이보연 <btothey99@ewhain.net>
2024-11-05 11:18:21 +0900

    feat: 커스텀 프롬프트 컨테이너 구현
    Jira: S11P31A210-431

---
+++ frontend/src/components/CustomPrompt/CustomPromptContainer.tsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import PrevPrompt from './PrevPrompt';
+import AlertModal from '@components/Modal/AlertModal';
+
+const CustomPromptContainer = () => {
+  const [content, setContent] = useState<string>('');
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState<boolean>(false);
+  const [alertMessage, setAlertMessage] = useState<string[]>([]);
+  const maxLength = 1000;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setContent(value);
+  };
+
+  const handleSavePrompt = () => {
+    if (maxLength && content.length > maxLength) {
+      setIsAlertModalOpen(true);
+      setAlertMessage(['최대 글자 수를 초과하였습니다.']);
+      return;
+    }
+    console.log(content);
+  };
+
+  return (
+    <>
+      <div className="flex flex-col gap-6">
+        <div className="relative w-full border-2 border-primary-500 rounded-xl p-4">
+          <PrevPrompt />
+          <textarea
+            className="w-full min-h-[400px] h-auto max-h-[420px] p-4 pb-8 rounded-2xl resize-none overflow-y-auto focus:outline-none bg-secondary text-white"
+            value={content}
+            onChange={handleChange}
+            placeholder="원하는 프롬프트를 입력해주세요."
+          />
+          {maxLength && (
+            <div
+              className={`absolute bottom-6 right-5 text-sm px-1 ${content.length >= maxLength ? 'text-state-warning bg-white rounded-2xl' : 'text-gray-600'}`}
+            >
+              {content.length} / {maxLength}
+            </div>
+          )}
+        </div>
+        <div className="flex justify-end">
+          <button
+            className="bg-primary-500 text-white px-4 py-2 rounded-xl w-fit"
+            onClick={handleSavePrompt}
+          >
+            저장
+          </button>
+        </div>
+      </div>
+      {isAlertModalOpen && (
+        <AlertModal content={alertMessage} onConfirm={() => setIsAlertModalOpen(false)} />
+      )}
+    </>
+  );
+};
+
+export default CustomPromptContainer;
