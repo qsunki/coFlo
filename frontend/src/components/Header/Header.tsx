ac2be268bf75fe4afe47fc67b62e341ded5f9473
이보연 <btothey99@ewhain.net>
2024-11-05 12:23:12 +0900

    refactor: 헤더 모듈화
    Jira: S11P31A210-431

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,12 +2,16 @@ export default function Header({ title, description }: HeaderProps) {
   return (
     <div className="flex flex-col space-y-2 mb-6 w-full max-w-[1000px] min-w-[350px] items-start font-pretendard">
       <div className="text-3xl font-bold">{title}</div>
-      {description &&
-        description.map((desc, index) => (
-          <div key={index} className="font-bold">
-            {desc}
-          </div>
-        ))}
+
+      {description && (
+        <div className="flex flex-col">
+          {description.map((desc, index) => (
+            <div key={index} className="font-bold">
+              {desc}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
