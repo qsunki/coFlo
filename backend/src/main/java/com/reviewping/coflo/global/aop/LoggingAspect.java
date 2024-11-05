ca9fffaf08f8ca739d7ef71f9990856f837d911d
손지민 <jimmi219@naver.com>
2024-11-05 17:19:46 +0900

    feat: 문자열 템플릿 추가

--- backend/src/main/java/com/reviewping/coflo/global/aop/LoggingAspect.java
+++ backend/src/main/java/com/reviewping/coflo/global/aop/LoggingAspect.java
@@ -46,18 +46,19 @@ public class LoggingAspect {
         StringBuilder messageBuilder = new StringBuilder();
         messageBuilder.append(
                 String.format(
-                        "Exception in %s.%s() called with arguments: `%s`\n",
+                        "```\nException in %s.%s() called with arguments: %s\n",
                         className, methodName, Arrays.toString(args)));
 
-        messageBuilder.append("**Exception message** : ").append(ex.getMessage()).append("\n");
+        messageBuilder.append("Exception message: ").append(ex.getMessage()).append("\n");
 
         if (ex instanceof BusinessException) {
             Throwable cause = ex.getCause();
             if (cause != null) {
-                messageBuilder.append("**Cause** : ").append(cause.getMessage()).append("\n");
+                messageBuilder.append("Cause: ").append(cause.getMessage()).append("\n");
             }
         }
 
+        messageBuilder.append("```");
         return messageBuilder.toString();
     }
 }
