b5b6354a0ec580748f9a2de247207312b25890c4
유승현 <fview@naver.com>
2024-11-05 13:18:51 +0900

    fix: 브랜치 조회 시 project 파라미터 추가
    Jira: S11P31A210-411

--- backend/src/main/java/com/reviewping/coflo/domain/project/repository/BranchRepository.java
+++ backend/src/main/java/com/reviewping/coflo/domain/project/repository/BranchRepository.java
@@ -1,16 +1,17 @@
 package com.reviewping.coflo.domain.project.repository;
 
 import com.reviewping.coflo.domain.project.entity.Branch;
+import com.reviewping.coflo.domain.project.entity.Project;
 import com.reviewping.coflo.global.error.ErrorCode;
 import com.reviewping.coflo.global.error.exception.BusinessException;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
-    Optional<Branch> findByName(String name);
+    Optional<Branch> findByNameAndProject(String name, Project project);
 
-    default Branch getByName(String name) {
-        return findByName(name)
+    default Branch getByNameAndProject(String name, Project project) {
+        return findByNameAndProject(name, project)
                 .orElseThrow(() -> new BusinessException(ErrorCode.BRANCH_NOT_FOUND));
     }
 }
