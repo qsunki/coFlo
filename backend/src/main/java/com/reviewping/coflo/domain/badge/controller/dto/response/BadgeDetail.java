9077c6044f5b3f3556e10c53061fdf7acd66312a
유승현 <fview@naver.com>
2024-11-05 17:17:24 +0900

    refactor: badgedetail dto 형식 변경
    Jira: S11P31A210-409

--- backend/src/main/java/com/reviewping/coflo/domain/badge/controller/dto/response/BadgeDetail.java
+++ backend/src/main/java/com/reviewping/coflo/domain/badge/controller/dto/response/BadgeDetail.java
@@ -2,12 +2,14 @@ package com.reviewping.coflo.domain.badge.controller.dto.response;
 
 import com.reviewping.coflo.domain.badge.entity.BadgeCode;
 
-public record BadgeDetail(Long badgeCodeId, String name, String description, String imageUrl) {
-    public static BadgeDetail of(BadgeCode badgeCode) {
+public record BadgeDetail(
+        Long badgeCodeId, String name, String description, String imageUrl, Boolean isAcquired) {
+    public static BadgeDetail of(BadgeCode badgeCode, Boolean isAcquired) {
         return new BadgeDetail(
                 badgeCode.getId(),
                 badgeCode.getName(),
                 badgeCode.getDescription(),
-                badgeCode.getImageUrl());
+                badgeCode.getImageUrl(),
+                isAcquired);
     }
 }
