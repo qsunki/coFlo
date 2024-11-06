9b650e4ccb7fa33d647bbe0a13eafb40a6921529
유승현 <fview@naver.com>
2024-11-06 13:55:20 +0900

    feat: merge request controller swagger operation 작성
    Jira: S11P31A210-409

--- backend/src/main/java/com/reviewping/coflo/domain/mergerequest/controller/MergeRequestController.java
+++ backend/src/main/java/com/reviewping/coflo/domain/mergerequest/controller/MergeRequestController.java
@@ -9,6 +9,7 @@ import com.reviewping.coflo.global.aop.LogExecution;
 import com.reviewping.coflo.global.auth.AuthUser;
 import com.reviewping.coflo.global.common.response.ApiResponse;
 import com.reviewping.coflo.global.common.response.impl.ApiSuccessResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +23,7 @@ public class MergeRequestController {
     private final MergeRequestService mergeRequestService;
 
     @GetMapping
+    @Operation(summary = "프로젝트의 merge request 목록 조회", description = "키워드 검색 가능, 상태 필터, 페이지네이션 제공")
     public ApiResponse<GitlabMrPageResponse> getGitlabMergeRequests(
             @AuthUser User user,
             @RequestParam(name = "projectId") Long projectId,
@@ -34,6 +36,7 @@ public class MergeRequestController {
     }
 
     @GetMapping("/best")
+    @Operation(summary = "프로젝트의 주간 Best MR 조회", description = "점수 가장 높은 상위 3개 항목 조회")
     public ApiResponse<List<GitlabMrResponse>> getBestGitlabMergeRequests(
             @AuthUser User user, @RequestParam(name = "projectId") Long projectId) {
         List<GitlabMrResponse> gitlabMrResponses =
