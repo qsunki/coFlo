29e1631380b1fc59c346777b66e12fcab9326cd6
이보연 <btothey99@ewhain.net>
2024-11-06 11:01:45 +0900

    feat: 401 에러 응답 처리
    Jira: S11P31A210-432

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -2,6 +2,7 @@ import axios, { AxiosResponse } from 'axios';
 import { useAtom } from 'jotai';
 import { errorAtom } from '@store/error';
 import { ApiResponse } from 'types/api';
+import { isLoginAtom } from '@store/auth';
 
 const instance = axios.create({
   baseURL: '/api',
@@ -11,12 +12,19 @@ const instance = axios.create({
 
 const useErrorHandler = () => {
   const [, setError] = useAtom(errorAtom);
+  const [, setIsLogin] = useAtom(isLoginAtom);
 
   const handleError = (error: any) => {
     let message = '서버와의 연결에 문제가 발생했습니다.';
 
     if (error.response) {
       const errorData = error.response.data;
+      console.log(error.response.status);
+      if (error.response.status === 401) {
+        setIsLogin(false);
+        window.location.href = '/login';
+        return;
+      }
 
       if (errorData && errorData.status === 'ERROR') {
         message = errorData.message || '알 수 없는 오류가 발생했습니다.';
