c20d6deed42f46a065aba02a6f261f657be011e4
이보연 <btothey99@ewhain.net>
2024-11-06 11:04:55 +0900

    로그인 로직 검사
    Jira: S11P31A210-432

--- frontend/src/pages/Login/LoginPage.tsx
+++ frontend/src/pages/Login/LoginPage.tsx
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAtom } from 'jotai';
+import { isLoginAtom, isSignupAtom, isConnectAtom } from '@store/auth';
 import LoginHeader from '@components/Login/LoginHeader.tsx';
 import LoginContent from '@components/Login/LoginContent.tsx';
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+  const [isLogin] = useAtom(isLoginAtom);
+  const [isSignup] = useAtom(isSignupAtom);
+  const [isConnect] = useAtom(isConnectAtom);
+
+  useEffect(() => {
+    console.log('로그인 페이지', isLogin, isSignup, isConnect);
+    if (!isLogin) return;
+    if (!isSignup) {
+      navigate('/signup');
+      return;
+    }
+
+    navigate(isConnect ? '/main' : '/repository');
+  }, [isLogin, isSignup, isConnect, navigate]);
+
   return (
     <div className="flex flex-row h-full w-full justify-center items-center min-h-screen">
       <div className="flex flex-col p-8">
