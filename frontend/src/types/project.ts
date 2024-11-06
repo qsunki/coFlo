51e0a93cd23fcb8bfef926f8deac73d117fe3489
이보연 <btothey99@ewhain.net>
2024-11-06 17:52:24 +0900

    feat: 프로젝트 응답 타입 설정
    Jira: S11P31A210-432

--- frontend/src/types/project.ts
+++ frontend/src/types/project.ts
@@ -8,3 +8,24 @@ export interface ProjectSelectorProps {
   onClose: () => void;
   titleRef: React.RefObject<HTMLDivElement>;
 }
+
+export interface ProjectLinkRequest {
+  botToken: string;
+  branches: string[];
+}
+
+export interface ApiResponseMapStringLong {
+  status: 'SUCCESS' | 'ERROR';
+  data: Record<string, number>;
+}
+
+export interface UserProjectResponse {
+  /** @format int64 */
+  projectId: number;
+  name: string;
+}
+
+export interface ApiResponseMapStringBoolean {
+  status: 'SUCCESS' | 'ERROR';
+  data: Record<string, boolean>;
+}
