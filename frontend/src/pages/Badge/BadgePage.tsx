f1b38b4e6b6a41775c0f5b3deb3ff720a9a30594
이보연 <btothey99@ewhain.net>
2024-11-07 16:20:59 +0900

    style: 안쓰는 코드 삭제
    Jira: S11P31A210-435

--- frontend/src/pages/Badge/BadgePage.tsx
+++ frontend/src/pages/Badge/BadgePage.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 
 import RepresentativeBadge from '@components/Badge/RepresentativeBadge';
 import BadgeList from '@components/Badge/BadgeList';
