05a4373c65bddedb4c23a3b2a64a4489d2df161f
이보연 <btothey99@ewhain.net>
2024-11-07 16:18:36 +0900

    remove: msw 파일 삭제
    Jira: S11P31A210-435

--- frontend/src/mocks/browser.ts
+++
@@ -1,6 +0,0 @@
-import { setupWorker } from 'msw';
-// import { handlers } from './bestMergeRequest.ts';
-import { handlers } from './gitlab';
-// import { handlers } from './handlers';
-
-export const worker = setupWorker(...handlers);
