d09ec03dbd34ce7cceeafa266de8bb7a6007a39c
유승현 <fview@naver.com>
2024-11-08 16:10:51 +0900

    feat: pageinfo dto 추가
    Jira: S11P31A210-407

---
+++ backend/src/main/java/com/reviewping/coflo/global/client/gitlab/response/PageInfo.java
@@ -0,0 +1,4 @@
+package com.reviewping.coflo.global.client.gitlab.response;
+
+public record PageInfo(
+        String startCursor, boolean hasNextPage, boolean hasPreviousPage, String endCursor) {}
