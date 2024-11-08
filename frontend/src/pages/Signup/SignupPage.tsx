5fe268344a043ffe66262e1a481b46968653352a
이보연 <btothey99@ewhain.net>
2024-11-08 11:04:38 +0900

    feat: 소셜 로그인 로직 수정
    Jira: S11P31A210-435

--- frontend/src/pages/Signup/SignupPage.tsx
+++ frontend/src/pages/Signup/SignupPage.tsx
@@ -1,9 +1,6 @@
 import SignupContent from '@components/Signup/SignupContent.tsx';
-import { useAuthRedirect } from '@hooks/useAuthRedirect';
 
 const SignupPage = () => {
-  useAuthRedirect();
-
   return (
     <div className="w-full h-screen flex justify-start items-center">
       <div className="w-full max-w-2xl p-5">
