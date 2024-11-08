e873a9ac0811d372f7a0396cf77a699ba27157f2
이보연 <btothey99@ewhain.net>
2024-11-08 16:14:12 +0900

    feat: MR 리스트 조회
    Jira: S11P31A210-435

--- frontend/src/pages/MergeList/MergeListPage.tsx
+++ frontend/src/pages/MergeList/MergeListPage.tsx
@@ -14,18 +14,25 @@ import { projectIdAtom } from '@store/auth';
 const MergeListPage = () => {
   const navigate = useNavigate();
   const [currentPage] = useAtom(currentPageAtom);
-  const [setTotalPages] = useAtom(totalPagesAtom);
+  const [, setTotalPages] = useAtom(totalPagesAtom);
   const [mergeRequests, setMergeRequests] = useState<GitlabMergeRequest[]>([]);
-  const itemsPerPage = 5;
+  const itemsPerPage = 7;
   const [projectId] = useAtom(projectIdAtom);
   const [currentStatus, setCurrentStatus] = useState('opened');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [, setSearchType] = useState('All');
 
   const handleStatusChange = (status: string) => {
     setCurrentStatus(status);
   };
 
+  const handleSearch = (keyword: string, searchType: string) => {
+    setSearchKeyword(keyword);
+    setSearchType(searchType);
+  };
+
   const handleItemClick = (id: number) => {
-    navigate(`/main/merge-request/reviews/${id}`);
+    navigate(`/${projectId}/main/merge-request/reviews/${id}`);
   };
 
   useEffect(() => {
@@ -34,12 +41,13 @@ const MergeListPage = () => {
         return;
       }
       const response = await MergeRequest.getMrList(projectId, currentStatus, {
-        keyword: '',
+        keyword: searchKeyword,
         page: Number(currentPage),
         size: Number(itemsPerPage),
       });
 
-      if (response && response.data) {
+      if (response.data) {
+        setTotalPages(response.data.totalPages ?? 1);
         setMergeRequests(response.data.gitlabMrList);
       }
     };
@@ -48,30 +56,30 @@ const MergeListPage = () => {
   }, [projectId, currentStatus, currentPage]);
 
   return (
-    <div className="flex flex-col flex-grow overflow-auto px-8 pt-6">
-      <div className="max-w-3xl  p-6">
+    <div className="flex flex-col flex-grow overflow-auto p-6 items-stretch justify-between w-full">
+      <div className="flex flex-col justify-between w-full">
         <MrStatusFilter onStatusChange={handleStatusChange} />
-        <CustomSearchBar></CustomSearchBar>
-
-        <div className="bg-white w-[1000px]">
-          {mergeRequests.length === 0 ? (
-            <EmptyMergeRequest />
-          ) : (
-            mergeRequests.map((mergeRequest) => (
-              <div
-                key={mergeRequest.id}
-                onClick={() => handleItemClick(mergeRequest.id)}
-                className="cursor-pointer"
-              >
-                <MrItem mergeRequest={mergeRequest} />
-                <div className="border-t border-gray-300" />
-              </div>
-            ))
-          )}
-        </div>
-
-        <Pagination />
+        <CustomSearchBar onSearch={handleSearch} />
       </div>
+
+      <div className="bg-white flex flex-col justify-between h-full py-4">
+        {mergeRequests.length === 0 ? (
+          <EmptyMergeRequest />
+        ) : (
+          mergeRequests.map((mergeRequest) => (
+            <div
+              key={mergeRequest.id}
+              onClick={() => handleItemClick(mergeRequest.id)}
+              className="cursor-pointer"
+            >
+              <MrItem mergeRequest={mergeRequest} />
+              <div className="border-t border-gray-300" />
+            </div>
+          ))
+        )}
+      </div>
+
+      <Pagination />
     </div>
   );
 };
