6f1f345d147aa00fe99280005a835ecc8ab8fd08
이보연 <btothey99@ewhain.net>
2024-11-08 14:22:38 +0900

    feat: 리스트가 비어있을 때 처리
    Jira: S11P31A210-435

--- frontend/src/pages/MergeList/MergeListPage.tsx
+++ frontend/src/pages/MergeList/MergeListPage.tsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAtom } from 'jotai';
-import { currentPageAtom, totalPagesAtom } from '@store/pagination';
 import { CustomSearchBar } from '@components/Mr/MrSearchBar';
 import { MrItem } from '@components/Mr/MrItem';
+import { MrStatusFilter } from '@components/MergeRequest/MrStatusFilter';
+import { EmptyMergeRequest } from '@components/MergeRequest/EmptyMergeRequest';
 import Pagination from '@components/Pagination/Pagination';
 import { MergeRequest } from '@apis/MergeRequest';
-import { useNavigate } from 'react-router-dom';
 import { GitlabMergeRequest } from 'types/mergeRequest';
+import { currentPageAtom, totalPagesAtom } from '@store/pagination';
+import { projectIdAtom } from '@store/auth';
 
 const MergeListPage = () => {
+  const navigate = useNavigate();
   const [currentPage] = useAtom(currentPageAtom);
   const [setTotalPages] = useAtom(totalPagesAtom);
   const [mergeRequests, setMergeRequests] = useState<GitlabMergeRequest[]>([]);
   const itemsPerPage = 5;
-  const navigate = useNavigate();
+  const [projectId] = useAtom(projectIdAtom);
+  const [currentStatus, setCurrentStatus] = useState('opened');
+
+  const handleStatusChange = (status: string) => {
+    setCurrentStatus(status);
+  };
 
   const handleItemClick = (id: number) => {
     navigate(`/main/merge-request/reviews/${id}`);
@@ -21,37 +30,44 @@ const MergeListPage = () => {
 
   useEffect(() => {
     const fetchMergeRequests = async () => {
-      const response = await MergeRequest.getMrList(
-        undefined,
-        String(Number(currentPage)),
-        String(Number(itemsPerPage)),
-      );
+      if (!projectId) {
+        return;
+      }
+      const response = await MergeRequest.getMrList(projectId, currentStatus, {
+        keyword: '',
+        page: Number(currentPage),
+        size: Number(itemsPerPage),
+      });
 
       if (response && response.data) {
         setMergeRequests(response.data.gitlabMrList);
-        // setTotalPages(response.data.totalPages);
       }
     };
 
     fetchMergeRequests();
-  }, [currentPage, setTotalPages]);
+  }, [projectId, currentStatus, currentPage]);
 
   return (
     <div className="flex flex-col flex-grow overflow-auto px-8 pt-6">
       <div className="max-w-3xl  p-6">
+        <MrStatusFilter onStatusChange={handleStatusChange} />
         <CustomSearchBar></CustomSearchBar>
 
         <div className="bg-white w-[1000px]">
-          {mergeRequests.map((mergeRequest) => (
-            <div
-              key={mergeRequest.id}
-              onClick={() => handleItemClick(mergeRequest.id)}
-              className="cursor-pointer"
-            >
-              <MrItem mergeRequest={mergeRequest} />
-              <div className="border-t border-gray-300" />
-            </div>
-          ))}
+          {mergeRequests.length === 0 ? (
+            <EmptyMergeRequest />
+          ) : (
+            mergeRequests.map((mergeRequest) => (
+              <div
+                key={mergeRequest.id}
+                onClick={() => handleItemClick(mergeRequest.id)}
+                className="cursor-pointer"
+              >
+                <MrItem mergeRequest={mergeRequest} />
+                <div className="border-t border-gray-300" />
+              </div>
+            ))
+          )}
         </div>
 
         <Pagination />
