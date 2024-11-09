6df921f8f33e58e4875cfa83181a66ee4ddcfbda
손지민 <jimmi219@naver.com>
2024-11-09 18:06:35 +0900

    refactor: 팩토리 구조 수정
    Jira: S11P31A210-495

--- backend/src/main/java/com/reviewping/coflo/domain/project/domain/caculator/ScoreCalculatorFactory.java
+++ backend/src/main/java/com/reviewping/coflo/domain/project/domain/caculator/ScoreCalculatorFactory.java
@@ -2,6 +2,8 @@ package com.reviewping.coflo.domain.project.domain.caculator;
 
 import com.reviewping.coflo.domain.project.domain.CalculationType;
 import com.reviewping.coflo.domain.project.domain.ScoreDisplayType;
+import com.reviewping.coflo.global.error.ErrorCode;
+import com.reviewping.coflo.global.error.exception.BusinessException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,9 +11,13 @@ public class ScoreCalculatorFactory {
 
     public ScoreCalculator get(CalculationType calculationType, ScoreDisplayType scoreDisplayType) {
         switch (scoreDisplayType) {
-            case INDIVIDUAL -> new IndividualScoreCalculator(calculationType);
-            case TOTAL -> new TotalScoreCalculator(calculationType);
+            case INDIVIDUAL -> {
+                return new IndividualScoreCalculator(calculationType);
+            }
+            case TOTAL -> {
+                return new TotalScoreCalculator(calculationType);
+            }
+            default -> throw new BusinessException(ErrorCode.PROJECT_STATISTICS_TYPE_NOT_EXIST);
         }
-        return null;
     }
 }
