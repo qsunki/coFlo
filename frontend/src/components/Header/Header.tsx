479fc0b643103552e4db504ad7937c9be917ae71
이보연 <btothey99@ewhain.net>
2024-11-11 11:05:56 +0900

    style: 헤더 css 수정
    Jira: S11P31A210-435

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ title, description }: HeaderProps) {
   return (
-    <div className="flex flex-col space-y-2 mb-6 w-full min-w-[1000px] max-w-[2000px] items-start font-pretendard">
+    <div className="flex flex-col space-y-2 mb-6 w-full min-w-[500px] max-w-[2000px] items-start font-pretendard">
       <div className="text-3xl font-bold">{title}</div>
 
       {description && (
