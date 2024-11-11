af997e2dd113adbc6064ad406b6b821dc39b6bf0
손지민 <jimmi219@naver.com>
2024-11-11 11:26:43 +0900

    feat: CORS PATCH 추가
    Jira: S11P31A210-566

--- backend/src/main/java/com/reviewping/coflo/global/config/SecurityConfig.java
+++ backend/src/main/java/com/reviewping/coflo/global/config/SecurityConfig.java
@@ -139,7 +139,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(
                 Arrays.asList("http://localhost:5173", "https://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(
+                Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
