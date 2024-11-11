a96bdd3627cefec13a7a0063bdf86f2a95ef8e41
anjs124 <87066209+anjs124@users.noreply.github.com>
2024-11-11 10:13:17 +0900

    feat : 전역 axios 에러 인터셉터 설정
    Jira: S11P31A210-464

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,7 @@
-// import ProjectCard from '@components/Project/ProjectCard';
-// import { Plus } from '@components/Plus/Plus';
-// import Navbar from '@components/Nav/Navbar/Navbar';
-// import { Sidebar } from '@components/Sidebar/Sidebar';
-// import { Outlet } from 'react-router-dom';
-// import CodeEditor from '@components/CodeEditor/CodeEditor';
+import { useAxiosInterceptor } from './config/error';
 
 const App = () => {
-  return (
-    // <div className="w-full bg-gray-100">
-    //   <div className="w-full mx-auto p-4">
-    //     <div className="flex justify-between items-center mb-6" style={{ marginLeft: '16px' }}>
-    //       <h1 className="text-2xl font-bold mr-4">내 연동된 프로젝트 목록</h1>
-    //       <Plus />
-    //     </div>
-
-    //     <ProjectCard />
-    //   </div>
-    // </div>
-    // // <div className="flex flex-row h-full w-full">
-    //   <Navbar></Navbar>
-    //   <Sidebar></Sidebar>
-    //   <Outlet />
-    // </div>
-
-    // <div className="flex  flex-row h-full w-full">
-    //   <Navbar></Navbar>
-    //   <RepositoryPage></RepositoryPage>
-    // </div>
-
-    <div className="flex  flex-row h-full w-full">
-      {/*<CodeEditor></CodeEditor>*/}
-    </div>
-  );
+  useAxiosInterceptor();
 };
 
 export default App;
--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,53 +1,23 @@
-import axios, { AxiosResponse } from 'axios';
-import { useAtom } from 'jotai';
-import { errorAtom } from '@store/error';
-import { ApiResponse } from 'types/api';
-import { isLoginAtom } from '@store/auth';
+import axios from 'axios';
 
 const VITE_API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const instance = axios.create({
-  baseURL: VITE_API_BASE_URL + '/api',
-  // baseURL: '/api',
+  baseURL: `${VITE_API_BASE_URL}/api`,
   timeout: 15000,
   withCredentials: true,
 });
 
-const useErrorHandler = () => {
-  const [, setError] = useAtom(errorAtom);
-  const [, setIsLogin] = useAtom(isLoginAtom);
-
-  const handleError = (error: any) => {
-    let message = '서버와의 연결에 문제가 발생했습니다.';
-
-    if (error.response) {
-      const errorData = error.response.data;
-      console.log(error.response.status);
-      if (error.response.status === 401) {
-        setIsLogin(false);
-        window.location.href = '/login';
-        return;
-      }
-
-      if (errorData && errorData.status === 'ERROR') {
-        message = errorData.message || '알 수 없는 오류가 발생했습니다.';
-      }
-    }
-
-    setError(message);
-    console.log(message);
-  };
-
-  return { handleError };
-};
-
 instance.interceptors.response.use(
-  (response: AxiosResponse<ApiResponse<any>>) => {
-    return response;
-  },
+  (response) => response,
   (error) => {
-    const { handleError } = useErrorHandler();
-    handleError(error);
+    console.log('Response error:', error);
+    if (error.response?.status === 401) {
+      console.log('401 Unauthorized - 로그인 페이지로 이동');
+
+      alert('로그인 정보가 만료되었습니다. 로그인 페이지로 이동합니다.');
+      window.location.href = '/login';
+    }
     return Promise.reject(error);
   },
 );
