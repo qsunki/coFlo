44b36f44378bfd8f18c2ef2778311c7efb7584a8
이보연 <btothey99@ewhain.net>
2024-11-12 17:29:34 +0900

    style: 메인 UI 확대해서 수정
    Jira: S11P31A210-509

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -149,11 +149,11 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="flex flex-col flex-grow overflow-auto p-4 gap-4 min-w-[1000px] min-h-[800px] max-w-[2000px] max-h-[1500px]">
+    <div className="flex flex-col flex-grow overflow-auto p-4 gap-8 min-w-[1000px] min-h-[700px] max-w-[2000px] max-h-[1200px]">
       {/* 첫 번째 행 */}
-      <div className="flex flex-row h-1/2 gap-4">
+      <div className="flex flex-row h-[45%] gap-4">
         {/* Team Radar Charts - 3/5 너비 */}
-        <div className="flex flex-col flex-[3] min-w-[570px] min-h-[500px]">
+        <div className="flex flex-col flex-[3] min-w-[570px] min-h-[400px]">
           <Title title="Team Radar Charts" textSize="text-lg" />
           <div
             ref={containerRef}
@@ -172,7 +172,7 @@ const HomePage = () => {
           </div>
         </div>
         {/* Project Total - 2/5 너비 */}
-        <div className="flex flex-col flex-[2] min-w-[380px] min-h-[500px]">
+        <div className="flex flex-col flex-[2] min-w-[380px] min-h-[400px]">
           <Title title="Project Total" textSize="text-lg" />
           <div className="flex-grow p-4 bg-gray-400 border-2 border-gray-500 rounded-lg">
             <Title title="About" textSize="text-xl" />
@@ -186,7 +186,7 @@ const HomePage = () => {
                   options={createHorizontalBarOptions([0, 100])}
                   chartId="horizontalBarChart"
                   width="100%"
-                  height="100%"
+                  height="60px"
                 />
               ) : (
                 <p className="text-center">No Chart</p>
@@ -229,9 +229,9 @@ const HomePage = () => {
       </div>
 
       {/* 두 번째 행 */}
-      <div className="flex flex-row h-1/2 gap-4">
+      <div className="flex flex-row h-[45%] gap-4">
         {/* Personal Growth Graph - 3/5 너비 */}
-        <div className="flex flex-col flex-[3] min-w-[570px] min-h-[500px]">
+        <div className="flex flex-col flex-[3] min-w-[570px] min-h-[400px]">
           <div className="flex flex-row justify-between items-center">
             <Title title="Personal Growth Graph" textSize="text-lg" />
             <ChartDropdown
@@ -248,7 +248,7 @@ const HomePage = () => {
                 data={cubicLineData}
                 options={createLineOptions([0, 70])}
                 chartId="lineChart"
-                height="400px"
+                height="300px"
               />
             )}
             {selectedChart === '획득 개별 스코어' && (
@@ -257,7 +257,7 @@ const HomePage = () => {
                 data={lineData}
                 options={createLineOptions([0, 11])}
                 chartId="cubicLineChart"
-                height="400px"
+                height="300px"
               />
             )}
             {selectedChart === '누적 통합 스코어' && (
@@ -266,7 +266,7 @@ const HomePage = () => {
                 data={cubicLineData}
                 options={createCubicLineOptions([minValue, maxValue])}
                 chartId="cumulativeLineChart"
-                height="400px"
+                height="300px"
               />
             )}
             {selectedChart === '누적 개별 스코어' && (
@@ -275,13 +275,13 @@ const HomePage = () => {
                 data={lineData}
                 options={createCubicLineOptions([minValue, maxValue])}
                 chartId="cumulativeCubicLineChart"
-                height="400px"
+                height="300px"
               />
             )}
           </div>
         </div>
         {/* Best Merge Request - 2/5 너비 */}
-        <div className="flex flex-col flex-[2] min-w-[380px] min-h-[500px]">
+        <div className="flex flex-col flex-[2] min-w-[380px] min-h-[400px]">
           <BestMergeRequestList />
         </div>
       </div>
