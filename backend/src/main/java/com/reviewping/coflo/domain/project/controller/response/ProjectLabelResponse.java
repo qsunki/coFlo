7bf62f952fa0b9544a92a8e3ab786f0c400d3857
유승현 <fview@naver.com>
2024-11-12 14:22:09 +0900

    feat: gitlab label color dto 생성
    Jira: S11P31A210-538

---
+++ backend/src/main/java/com/reviewping/coflo/domain/project/controller/response/ProjectLabelResponse.java
@@ -0,0 +1,15 @@
+package com.reviewping.coflo.domain.project.controller.response;
+
+import com.reviewping.coflo.global.client.gitlab.response.GitlabLabelColorContent;
+import java.util.List;
+
+public record ProjectLabelResponse(List<LabelInfo> labels) {
+    public static ProjectLabelResponse of(List<GitlabLabelColorContent> labels) {
+        return new ProjectLabelResponse(
+                labels.stream()
+                        .map(label -> new LabelInfo(label.name(), label.textColor(), label.color()))
+                        .toList());
+    }
+
+    private record LabelInfo(String name, String textColor, String bgColor) {}
+}
