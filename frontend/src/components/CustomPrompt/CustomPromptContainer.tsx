369817a93ded7299c79a744f8d7f1e6c81a6b82a
anjs124 <87066209+anjs124@users.noreply.github.com>
2024-11-12 10:29:03 +0900

    feat : 커스텀 프롬프트 변경
    Jira: S11P31A210-525

--- frontend/src/components/CustomPrompt/CustomPromptContainer.tsx
+++ frontend/src/components/CustomPrompt/CustomPromptContainer.tsx
@@ -42,31 +42,32 @@ const CustomPromptContainer = () => {
   return (
     <>
       <div className="flex flex-col gap-6 min-w-[600px]">
-        <div className="relative w-full border-2 border-primary-500 rounded-xl p-4">
+        <div className="relative w-full border-1 border-primary-500 shadow-lg rounded-xl p-4 bg-[#EFF2FB] min-h-[300px]">
           <PrevPrompt />
-          <textarea
-            className="w-full min-h-[400px] h-auto max-h-[420px] p-4 pb-8 rounded-2xl resize-none overflow-y-auto focus:outline-none bg-secondary text-white"
-            value={content}
-            onChange={handleChange}
-            placeholder="원하는 프롬프트를 입력해주세요."
-          />
-          {maxLength && (
-            <div
-              className={`absolute bottom-6 right-5 text-sm px-1 ${content.length >= maxLength ? 'text-state-warning bg-white rounded-2xl' : 'text-gray-600'}`}
-            >
-              {content.length} / {maxLength}
-            </div>
-          )}
         </div>
-        <div className="flex justify-end">
-          <button
-            className="bg-primary-500 text-white px-4 py-2 rounded-xl w-fit"
-            onClick={handleSavePrompt}
+        <textarea
+          className="w-full min-h-[400px] h-auto max-h-[420px] border-1 border-primary-500 shadow-lg p-4 pb-8 rounded-2xl resize-none overflow-y-auto focus:outline-none bg-[#F5F7FA] placeholder:text-[#A5A5A5]"
+          value={content}
+          onChange={handleChange}
+          placeholder="원하는 프롬프트를 입력해주세요."
+        />
+        {maxLength && (
+          <div
+            className={`absolute bottom-6 right-5 text-sm px-1 ${content.length >= maxLength ? 'text-state-warning bg-white rounded-2xl' : 'text-gray-600'}`}
           >
-            저장
-          </button>
-        </div>
+            {content.length} / {maxLength}
+          </div>
+        )}
       </div>
+      <div className="flex mt-5 justify-end">
+        <button
+          className="bg-primary-500 text-white px-4 py-2 rounded-xl w-fit"
+          onClick={handleSavePrompt}
+        >
+          저장
+        </button>
+      </div>
+
       {isAlertModalOpen && (
         <AlertModal content={alertMessage} onConfirm={() => setIsAlertModalOpen(false)} />
       )}
