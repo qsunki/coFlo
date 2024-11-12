991d19ef1f84d6ea8960a2ed208df08c1fb6b296
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-11-12 13:24:46 +0900

    refactor: 불필요한 코드 제거
    Jira: S11P31A210-554

--- backend/src/main/java/com/reviewping/coflo/domain/notification/api/SSEController.java
+++ backend/src/main/java/com/reviewping/coflo/domain/notification/api/SSEController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
-@RequestMapping("/sse")
+@RequestMapping("/api/sse")
 @RequiredArgsConstructor
 public class SSEController {
     private final SSEService sseService;
--- backend/src/main/java/com/reviewping/coflo/domain/notification/service/SSEService.java
+++ backend/src/main/java/com/reviewping/coflo/domain/notification/service/SSEService.java
@@ -5,25 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reviewping.coflo.domain.notification.repository.EmitterRepository;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Service
 @RequiredArgsConstructor
 public class SSEService {
-    private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
+    private static final long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
     private final EmitterRepository emitterRepository;
+    private final ObjectMapper objectMapper;
 
-    @Autowired private ObjectMapper objectMapper;
-
-    /**
-     * 클라이언트가 구독을 위해 호출하는 메서드.
-     *
-     * @param userId - 구독하는 클라이언트의 사용자 아이디.
-     * @return SseEmitter - 서버에서 보낸 이벤트 Emitter
-     */
     public SseEmitter subscribe(Long userId) {
         SseEmitter emitter = createEmitter(userId);
 
@@ -31,23 +23,11 @@ public class SSEService {
         return emitter;
     }
 
-    /**
-     * 서버의 이벤트를 클라이언트에게 보내는 메서드
-     *
-     * @param userId - 메세지를 전송할 사용자의 아이디.
-     * @param event  - 전송할 이벤트 객체.
-     */
     public void notify(Long userId, Object event) {
         System.out.println("Notify method called with userId: " + userId + " and event: " + event);
         sendToClient(userId, event);
     }
 
-    /**
-     * 클라이언트에게 데이터를 전송
-     *
-     * @param id   - 데이터를 받을 사용자의 아이디.
-     * @param data - 전송할 데이터.
-     */
     private void sendToClient(Long id, Object data) {
         SseEmitter emitter = emitterRepository.get(id);
         if (emitter != null) {
@@ -60,11 +40,9 @@ public class SSEService {
                                 .name("notification")
                                 .data(jsonData));
             } catch (JsonProcessingException e) {
-                // JSON 변환 오류
                 e.printStackTrace();
                 throw new RuntimeException("JSON 변환 오류!", e);
             } catch (IOException e) {
-                // I/O 오류
                 e.printStackTrace();
                 emitterRepository.deleteById(id);
                 throw new RuntimeException("연결 오류!", e);
@@ -74,21 +52,12 @@ public class SSEService {
         }
     }
 
-    /**
-     * 사용자 아이디를 기반으로 이벤트 Emitter를 생성
-     *
-     * @param id - 사용자 아이디.
-     * @return SseEmitter - 생성된 이벤트 Emitter.
-     */
     private SseEmitter createEmitter(Long id) {
         SseEmitter emitter = new SseEmitter(DEFAULT_TIMEOUT);
         emitterRepository.save(id, emitter);
 
-        // Emitter가 완료될 때(모든 데이터가 성공적으로 전송된 상태) Emitter를 삭제한다.
         emitter.onCompletion(() -> emitterRepository.deleteById(id));
-        // Emitter가 타임아웃 되었을 때(지정된 시간동안 어떠한 이벤트도 전송되지 않았을 때) Emitter를 삭제한다.
         emitter.onTimeout(() -> emitterRepository.deleteById(id));
-
         return emitter;
     }
 }
