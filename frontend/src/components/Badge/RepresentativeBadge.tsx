6db20a3c4d4bac1e4e329e328f10bbea49a5a6a7
이보연 <btothey99@ewhain.net>
2024-11-13 16:47:25 +0900

    feat: 뱃지 페이지 레이아웃 수정
    Jira: S11P31A210-509

--- frontend/src/components/Badge/RepresentativeBadge.tsx
+++ frontend/src/components/Badge/RepresentativeBadge.tsx
@@ -3,9 +3,10 @@ import { RepresentativeBadgeProps } from 'types/badge';
 const RepresentativeBadge = ({ selectedBadge, onSave }: RepresentativeBadgeProps) => {
   return (
     <div className="my-8 flex flex-col items-center ">
-      <div className="w-52 h-52 bg-primary-500 border-2 border-primary-500 rounded-lg flex items-center justify-center mb-4">
+      <div className="text-xl font-extrabold mb-4">대표 뱃지</div>
+      <div className="w-60 h-60 bg-[#EFF2FB] border-2 border-[#EAEEF8] rounded-full flex items-center justify-center mb-4">
         {selectedBadge ? (
-          <img src={selectedBadge} alt="대표 뱃지" className="w-40 h-40 object-contain" />
+          <img src={selectedBadge} alt="대표 뱃지" className="w-48 h-48 object-contain" />
         ) : (
           <div className="flex flex-col items-center text-white">
             <span>대표 뱃지를</span> <span> 선택해주세요</span>
@@ -13,7 +14,10 @@ const RepresentativeBadge = ({ selectedBadge, onSave }: RepresentativeBadgeProps
         )}
       </div>
       <div className="flex flex-row gap-4">
-        <button onClick={onSave} className="px-4 py-2 bg-primary-500 text-white rounded-xl text-xl">
+        <button
+          onClick={onSave}
+          className="px-4 py-2 bg-secondary text-white font-bold rounded-xl text-lg"
+        >
           저장
         </button>
       </div>
