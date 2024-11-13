15448719e66a214b4688aa6350147ee7cce7b5be
손지민 <jimmi219@naver.com>
2024-11-13 13:27:03 +0900

    feat: typescript 청크 로직 수정

--- ai/src/main/java/com/reviewping/coflo/treesitter/strategy/TypeScriptChunkStrategy.java
+++ ai/src/main/java/com/reviewping/coflo/treesitter/strategy/TypeScriptChunkStrategy.java
@@ -4,6 +4,7 @@ import com.reviewping.coflo.service.dto.ChunkedCode;
 import com.reviewping.coflo.treesitter.FileUtil;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.treesitter.*;
 
@@ -23,18 +24,33 @@ public class TypeScriptChunkStrategy implements ChunkStrategy {
         TSTree tree = parser.parseString(null, new String(code));
         TSNode rootNode = tree.getRootNode();
         List<ChunkedCode> chunks = new ArrayList<>();
-
-        for (int i = 0; i < rootNode.getChildCount(); i++) {
-            TSNode node = rootNode.getChild(i);
-            if ("function_declaration".equals(node.getType())
-                    || "interface_declaration".equals(node.getType())) {
-                String blockCode =
-                        new String(
-                                code, node.getStartByte(), node.getEndByte() - node.getStartByte());
-                chunks.add(
-                        new ChunkedCode(blockCode, file.getName(), file.getPath(), "typescript"));
-            }
-        }
+        traverseAndCollectNodes(rootNode, chunks, code, file);
         return chunks;
+    }
+
+    private void traverseAndCollectNodes(
+            TSNode node, List<ChunkedCode> chunks, byte[] code, File file) {
+
+        if ("class_declaration".equals(node.getType())) {
+            String classCode =
+                    new String(Arrays.copyOfRange(code, node.getStartByte(), node.getEndByte()));
+            chunks.add(new ChunkedCode(classCode, file.getName(), file.getPath(), "typescript"));
+            return;
+        } else if ("function_declaration".equals(node.getType())) {
+            String functionCode =
+                    new String(Arrays.copyOfRange(code, node.getStartByte(), node.getEndByte()));
+            chunks.add(new ChunkedCode(functionCode, file.getName(), file.getPath(), "typescript"));
+            return;
+        } else if ("lexical_declaration".equals(node.getType())) {
+            String lexicalCode =
+                    new String(Arrays.copyOfRange(code, node.getStartByte(), node.getEndByte()));
+            chunks.add(new ChunkedCode(lexicalCode, file.getName(), file.getPath(), "typescript"));
+            return;
+        }
+
+        for (int i = 0; i < node.getChildCount(); i++) {
+            TSNode childNode = node.getChild(i);
+            traverseAndCollectNodes(childNode, chunks, code, file);
+        }
     }
 }
