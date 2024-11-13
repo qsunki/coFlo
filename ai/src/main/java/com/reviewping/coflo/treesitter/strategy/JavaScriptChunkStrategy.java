e827427335653d880f88b3f7e7a62b57215ac619
손지민 <jimmi219@naver.com>
2024-11-13 10:59:17 +0900

    test: 자바스크립트 청크 테스트 구현

--- ai/src/main/java/com/reviewping/coflo/treesitter/strategy/JavaScriptChunkStrategy.java
+++ ai/src/main/java/com/reviewping/coflo/treesitter/strategy/JavaScriptChunkStrategy.java
@@ -6,10 +6,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.treesitter.*;
 
 public class JavaScriptChunkStrategy implements ChunkStrategy {
 
+    private static final Logger log = LoggerFactory.getLogger(JavaScriptChunkStrategy.class);
     private final TSParser parser;
 
     public JavaScriptChunkStrategy(TSParser parser) {
@@ -24,19 +27,38 @@ public class JavaScriptChunkStrategy implements ChunkStrategy {
         TSTree tree = parser.parseString(null, new String(code));
         TSNode rootNode = tree.getRootNode();
         List<ChunkedCode> chunks = new ArrayList<>();
+        traverseAndCollectNodes(rootNode, chunks, code, file);
+        return chunks;
+    }
 
-        for (int i = 0; i < rootNode.getChildCount(); i++) {
-            TSNode node = rootNode.getChild(i);
-            if ("function_declaration".equals(node.getType())
-                    || "method_definition".equals(node.getType())) {
+    private void traverseAndCollectNodes(
+            TSNode node, List<ChunkedCode> chunks, byte[] code, File file) {
+        if ("function_declaration".equals(node.getType())) {
+            String functionCode =
+                    new String(Arrays.copyOfRange(code, node.getStartByte(), node.getEndByte()));
+            chunks.add(new ChunkedCode(functionCode, file.getName(), file.getPath(), "javascript"));
+        } else if ("variable_declarator".equals(node.getType())) {
+            boolean hasArrowFunction = false;
+            TSNode parent = node.getParent();
+            for (int i = 0; i < node.getChildCount(); i++) {
+                TSNode child = node.getChild(i);
+                if ("arrow_function".equals(child.getType())) {
+                    hasArrowFunction = true;
+                    break;
+                }
+            }
+            if (hasArrowFunction && parent != null) {
                 String functionCode =
                         new String(
-                                Arrays.copyOfRange(code, node.getStartByte(), node.getEndByte()));
+                                Arrays.copyOfRange(
+                                        code, parent.getStartByte(), parent.getEndByte()));
                 chunks.add(
                         new ChunkedCode(
                                 functionCode, file.getName(), file.getPath(), "javascript"));
             }
         }
-        return chunks;
+        for (int i = 0; i < node.getChildCount(); i++) {
+            traverseAndCollectNodes(node.getChild(i), chunks, code, file);
+        }
     }
 }
