33ba0527cf5deab18ff407a463f7800b0667b9ad
손지민 <jimmi219@naver.com>
2024-11-14 09:46:02 +0900

    test: 청크 기본 전략 테스트 추가

---
+++ ai/src/test/java/com/reviewping/coflo/treesitter/strategy/BaseChunkStrategyTest.java
@@ -0,0 +1,42 @@
+package com.reviewping.coflo.treesitter.strategy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.reviewping.coflo.service.dto.ChunkedCode;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class BaseChunkStrategyTest {
+
+    private BaseChunkStrategy strategy;
+    private File htmlFile;
+
+    @BeforeEach
+    public void setUp() throws IOException {
+        strategy = new BaseChunkStrategy();
+        htmlFile = Paths.get("src/test/resources/TestHTML.html").toFile();
+    }
+
+    private String normalize(String input) {
+        return input.replaceAll("\\s+", " ").trim();
+    }
+
+    @Test
+    public void testChunkingHTMLFile() {
+        List<ChunkedCode> chunks = strategy.chunk(htmlFile);
+
+        System.out.println("\nchunks size: " + chunks.size());
+        for (int i = 0; i < chunks.size(); i++) {
+            System.out.println("[Chunk " + i + " content] \n" + chunks.get(i).getContent() + "\n");
+        }
+
+        assertEquals(1, chunks.size(), "Expected 1 chunks to be extracted.");
+
+        assertEquals(htmlFile.getName(), chunks.get(0).getFileName());
+        assertEquals("html", chunks.get(0).getLanguage());
+    }
+}
