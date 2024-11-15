b9c02a1df6c4c067318914a7086a93c2a3b178c3
qsunki <ajsthfldu@outlook.com>
2024-11-15 09:10:32 +0900

    feat: 상세리뷰 gateway 추가
    Jira: S11P31A210-549

--- ai/src/main/java/com/reviewping/coflo/gateway/RedisGateway.java
+++ ai/src/main/java/com/reviewping/coflo/gateway/RedisGateway.java
@@ -1,5 +1,6 @@
 package com.reviewping.coflo.gateway;
 
+import com.reviewping.coflo.service.dto.response.DetailedReviewResponseMessage;
 import com.reviewping.coflo.service.dto.response.MrEvalResponseMessage;
 import com.reviewping.coflo.service.dto.response.ReviewResponseMessage;
 import org.springframework.integration.annotation.Gateway;
@@ -12,9 +13,9 @@ public interface RedisGateway {
     @Gateway(headers = @GatewayHeader(name = "topic", value = "review-response"))
     void sendReview(ReviewResponseMessage reviewResponse);
 
+    @Gateway(headers = @GatewayHeader(name = "topic", value = "detailed-review-response"))
+    void sendReview(DetailedReviewResponseMessage reviewResponse);
+
     @Gateway(headers = @GatewayHeader(name = "topic", value = "mr-eval-response"))
     void sendEval(MrEvalResponseMessage evalResponse);
-
-    @Gateway(headers = @GatewayHeader(name = "topic", value = "test"))
-    void sendTest(String message);
 }
