ad8ab4f534fd798e1c366db06b19c87a52756d9c
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2024-11-15 19:08:55 +0900

    feat: imageUrl 속성 추가

--- backend/src/main/java/com/reviewping/coflo/domain/webhookchannel/controller/dto/response/ChannelCodeResponse.java
+++ backend/src/main/java/com/reviewping/coflo/domain/webhookchannel/controller/dto/response/ChannelCodeResponse.java
@@ -2,8 +2,9 @@ package com.reviewping.coflo.domain.webhookchannel.controller.dto.response;
 
 import com.reviewping.coflo.domain.webhookchannel.entity.ChannelCode;
 
-public record ChannelCodeResponse(Long channelCodeId, String channelName) {
+public record ChannelCodeResponse(Long channelCodeId, String channelName, String imageUrl) {
     public static ChannelCodeResponse of(ChannelCode channelCode) {
-        return new ChannelCodeResponse(channelCode.getId(), channelCode.getName().name());
+        return new ChannelCodeResponse(
+                channelCode.getId(), channelCode.getName().name(), channelCode.getImageUrl());
     }
 }
