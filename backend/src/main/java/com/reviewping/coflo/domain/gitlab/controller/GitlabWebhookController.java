e187871622d22e0326287051c0167d069cd30a3b
qsunki <ajsthfldu@outlook.com>
2024-11-18 00:35:00 +0900

    refactor: 웹훅 컨트롤러 개선

--- backend/src/main/java/com/reviewping/coflo/domain/gitlab/controller/GitlabWebhookController.java
+++ backend/src/main/java/com/reviewping/coflo/domain/gitlab/controller/GitlabWebhookController.java
@@ -1,14 +1,10 @@
 package com.reviewping.coflo.domain.gitlab.controller;
 
-import static com.reviewping.coflo.global.error.ErrorCode.UNSUPPORTED_EVENT_TYPE;
-
 import com.reviewping.coflo.domain.gitlab.service.GitlabEventHandler;
 import com.reviewping.coflo.global.aop.LogExecution;
 import com.reviewping.coflo.global.client.gitlab.request.GitlabEventRequest;
 import com.reviewping.coflo.global.common.response.ApiResponse;
-import com.reviewping.coflo.global.common.response.impl.ApiErrorResponse;
 import com.reviewping.coflo.global.common.response.impl.ApiSuccessResponse;
-import com.reviewping.coflo.global.error.exception.BusinessException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,31 +14,14 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class GitlabWebhookController {
 
-    private static final String MERGE_REQUEST_TYPE = "Merge Request Hook";
-    private static final String PUSH_REQUEST_TYPE = "Push Hook";
-
     private final GitlabEventHandler gitlabEventHandler;
 
-    @PostMapping("/webhook/{projectId}")
+    @PostMapping(value = "/webhook/{projectId}", headers = "X-Gitlab-Event=Merge Request Hook")
     @Operation(summary = "Gitlab webhook 등록 시 사용")
     public ApiResponse<Void> handleGitlabEvent(
-            @RequestHeader("X-Gitlab-Event") String gitlabEventType,
             @PathVariable("projectId") Long projectId,
             @RequestBody GitlabEventRequest gitlabEventRequest) {
-        return switch (gitlabEventType) {
-            case MERGE_REQUEST_TYPE -> {
-                try {
-                    gitlabEventHandler.handleMergeRequest(projectId, gitlabEventRequest);
-                } catch (BusinessException e) {
-                    yield ApiErrorResponse.error(e.getErrorCode());
-                }
-                yield ApiSuccessResponse.success();
-            }
-            case PUSH_REQUEST_TYPE -> {
-                gitlabEventHandler.handlePush(projectId, gitlabEventRequest);
-                yield ApiSuccessResponse.success();
-            }
-            default -> ApiErrorResponse.error(UNSUPPORTED_EVENT_TYPE);
-        };
+        gitlabEventHandler.handleMergeRequest(projectId, gitlabEventRequest);
+        return ApiSuccessResponse.success();
     }
 }
