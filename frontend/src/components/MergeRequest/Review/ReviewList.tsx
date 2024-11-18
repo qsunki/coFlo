7205cd417eaa4933ea9fc2b4b6df16109bac98c6
이보연 <btothey99@ewhain.net>
2024-11-18 14:03:42 +0900

    style: 빈 값 출력
    Jira: S11P31A210-586

--- frontend/src/components/MergeRequest/Review/ReviewList.tsx
+++ frontend/src/components/MergeRequest/Review/ReviewList.tsx
@@ -14,15 +14,19 @@ const ReviewList = ({
   return (
     <div className="font-pretendard flex-[4] pl-4 min-w-[500px] relative">
       <div className="absolute top-0 left-[1rem] h-full border-l-[1px] border-background-bnavy" />
-      {reviews.map((review, index) => (
-        <div
-          style={{ cursor: 'pointer' }}
-          key={index}
-          onClick={() => onReviewClick(String(Number(review.id)))}
-        >
-          <ReviewItem review={review} mergeRequest={mergeRequest} />
-        </div>
-      ))}
+      {reviews.length === 0 ? (
+        <div className="text-gray-700 text-center p-4 mt-16">아직 리뷰가 생성되지 않았습니다.</div>
+      ) : (
+        reviews.map((review, index) => (
+          <div
+            style={{ cursor: 'pointer' }}
+            key={index}
+            onClick={() => onReviewClick(String(Number(review.id)))}
+          >
+            <ReviewItem review={review} mergeRequest={mergeRequest} />
+          </div>
+        ))
+      )}
     </div>
   );
 };
